452689e4221299b8982d60fc04c90a6cea569a17
Michael Hennerich <michael.hennerich@analog.com>
2007-01-17 15:35:58 +0000

    Fix BUG[#1787], add support for new gpio driver, add proper locking, cleanup
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@2664 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/char/bfin_pflags.c
+++ drivers/char/bfin_pflags.c
@@ -62,33 +62,16 @@
 #include <linux/interrupt.h>
 #include <asm/uaccess.h>
 #include <asm/blackfin.h>
+#include <asm/gpio.h>
 #include <linux/proc_fs.h>
+#include <linux/spinlock.h>
 #include "bfin_pflags.h"
 
-#undef	DEBUG
-//#define DEBUG
-
-#undef ENABLE_POLL
-//#define ENABLE_POLL
-
-#ifdef DEBUG
-# define DPRINTK(x...)	printk(KERN_DEBUG x)
-#else
-# define DPRINTK(x...)	do { } while (0)
-#endif
 
 #define PFLAG_MAJOR 253		//experimental
 
-/* 0 / 1 meaning 0=input, 1=output *
- *                111111           *
- *                5432109876543210 */
-#define PINCONF 0b0000000000011100
+static DEFINE_SPINLOCK(pflags_lock);
 
-#ifdef ENABLE_POLL
-static wait_queue_head_t pflags_in_waitq;
-static short pflags_laststate = 0;
-static short pflags_statechanged = 0;
-#endif
 
 /* return the minor number or -ENODEV */
 
@@ -96,7 +79,7 @@ static int check_minor(struct inode *inode)
 {
 	int minor = MINOR(inode->i_rdev);
 
-	if (minor > 15)
+	if (minor > MAX_BLACKFIN_GPIOS)
 		return -ENODEV;
 
 	return minor;
@@ -129,20 +112,40 @@ static int check_minor(struct inode *inode)
 **************************************************************/
 static int pflags_open(struct inode *inode, struct file *filp)
 {
+	unsigned long flags;
+
+	int minor = MINOR(inode->i_rdev);
+
 	if (check_minor(inode) < 0)
 		return -ENODEV;
 
-#if defined(CONFIG_BF534) || defined(CONFIG_BF536) || defined(CONFIG_BF537)
-	bfin_write_PORT_FER(bfin_read_PORT_FER() & ~(1 << MINOR(inode->i_rdev)));
-#endif
+	spin_lock_irqsave(&pflags_lock, flags);
+	
+	if(gpio_request(minor, NULL)){
+		spin_unlock_irqrestore(&pflags_lock, flags);
+		return -EBUSY;
+	}
+		
+	spin_unlock_irqrestore(&pflags_lock, flags);
 
 	return 0;
 }
 
 static int pflags_release(struct inode *inode, struct file *filp)
 {
+	unsigned long flags;
+
+	int minor = MINOR(inode->i_rdev);
+
 	if (check_minor(inode) < 0)
 		return -ENODEV;
+
+	spin_lock_irqsave(&pflags_lock, flags);
+	
+	gpio_free(minor);
+
+	spin_unlock_irqrestore(&pflags_lock, flags);
+
 	return 0;
 }
 
@@ -178,7 +181,7 @@ static ssize_t pflags_read(struct file *filp, char *buf, size_t size, loff_t * o
 	const char *bit;
 	int minor = check_minor(filp->f_dentry->d_inode);
 
-	DPRINTK("pfbits driver for bf53x minor = %d\n", minor);
+	pr_debug("pfbits driver for bf53x minor = %d\n", minor);
 
 	if (minor < 0)
 		return -ENODEV;
@@ -186,7 +189,9 @@ static ssize_t pflags_read(struct file *filp, char *buf, size_t size, loff_t * o
 	if (size < 2)
 		return -EMSGSIZE;
 
-	bit = (bfin_read_FIO_FLAG_D() & (1 << minor)) ? "1" : "0";
+	gpio_direction_input(minor);
+	
+	bit = gpio_get_value(minor) ? "1" : "0";
 
 	return (copy_to_user(buf, bit, 2)) ? -EFAULT : 2;
 }
@@ -225,9 +230,7 @@ static ssize_t pflags_write(struct file *filp, const char *buf, size_t size, lof
 {
 	int minor = check_minor(filp->f_dentry->d_inode);
 
-	volatile unsigned short *set_or_clear;
-
-	DPRINTK("pfbits driver for bf53x minor = %d\n", minor);
+	pr_debug("pfbits driver for bf53x minor = %d\n", minor);
 
 	if (minor < 0)
 		return -ENODEV;
@@ -238,74 +241,14 @@ static ssize_t pflags_write(struct file *filp, const char *buf, size_t size, lof
 	if (!buf)
 		return -EFAULT;
 
-	set_or_clear = (buf[0] == '0') ?
-		((volatile unsigned short *)FIO_FLAG_C) :
-		((volatile unsigned short *)FIO_FLAG_S);
+	gpio_direction_output(minor);
+	
+	gpio_set_value(minor, buf[0] == '0' ? 0 : 1);
 
-	*set_or_clear = (1 << minor);
 
 	return size;
 
 }
-
-#ifdef ENABLE_POLL
-static unsigned int pflags_poll(struct file *filp,
-				struct poll_table_struct *wait)
-{
-	int minor = check_minor(filp->f_dentry->d_inode);
-
-	int changed = 0;
-
-	unsigned int mask = 0;
-
-	if (minor < 0)
-		return -ENODEV;
-
-//	bfin_write_FIO_MASKA_C(bfin_read_FIO_MASKA_C() | (1 << minor));
-	bfin_write_FIO_MASKA_S(bfin_read_FIO_MASKA_S() | (1 << minor));
-	bfin_write_FIO_INEN(bfin_read_FIO_INEN() | (1 << minor));
-
-	if (filp->f_mode & FMODE_READ) {
-
-		do {
-
-			/* attention! this wakes up when /any/ of the flags changes */
-			poll_wait(filp, &pflags_in_waitq, wait);
-
-			changed = pflags_statechanged & (1 << minor);
-
-		} while (!changed);
-
-		mask |= POLLIN | POLLRDNORM;
-
-	}
-
-	/* we can always write */
-	if (filp->f_mode & FMODE_WRITE)
-		mask |= POLLOUT | POLLWRNORM;
-
-	return mask;
-}
-
-static irqreturn_t pflags_irq_handler(int irq, void *dev_id,
-				      struct pt_regs *regs)
-{
-	short pflags_nextstate;
-
-	pflags_nextstate = bfin_read_FIO_FLAG_D();
-	/* FIXME: Clear only status of flag pin that caused the interrupt */
-	bfin_write_FIO_FLAG_C(0xFFFF);	/* clear irq status on interrupt lines */
-
-	DPRINTK("pflags_irq_handler\n");
-
-	pflags_statechanged = pflags_laststate ^ pflags_nextstate;
-	pflags_laststate = pflags_nextstate;
-
-	wake_up(&pflags_in_waitq);
-
-	return IRQ_HANDLED;
-}
-#endif
 
 /*
  *  Info exported via "/proc/driver/pflags".
@@ -314,37 +257,20 @@ static irqreturn_t pflags_irq_handler(int irq, void *dev_id,
 static int pflags_proc_output(char *buf)
 {
 	char *p;
-	unsigned short i, data, dir, maska, maskb, polar, edge, inen, both;
+	unsigned short i;
 	p = buf;
 
-	data = bfin_read_FIO_FLAG_D();
-	dir = bfin_read_FIO_DIR();
-	maska = bfin_read_FIO_MASKA_D();
-	maskb = bfin_read_FIO_MASKB_D();
-	polar = bfin_read_FIO_POLAR();
-	both = bfin_read_FIO_BOTH();
-	edge = bfin_read_FIO_EDGE();
-	inen = bfin_read_FIO_INEN();
 
-	p += sprintf(p, "FIO_DIR \t: = 0x%X\n", dir);
-	p += sprintf(p, "FIO_MASKA\t: = 0x%X\n", maska);
-	p += sprintf(p, "FIO_MASKB\t: = 0x%X\n", maskb);
-	p += sprintf(p, "FIO_POLAR\t: = 0x%X\n", polar);
-	p += sprintf(p, "FIO_EDGE \t: = 0x%X\n", edge);
-	p += sprintf(p, "FIO_INEN \t: = 0x%X\n", inen);
-	p += sprintf(p, "FIO_BOTH \t: = 0x%X\n", both);
-	p += sprintf(p, "FIO_FLAG_D\t: = 0x%X\n", data);
 	p += sprintf(p, "PIN\t:DATA DIR INEN EDGE BOTH POLAR MASKA MASKB\n");
-	p += sprintf(p, "   \t:H/L  O/I D/E  E/L  B/S   L/H   S/C   S/C\n");
+	p += sprintf(p, "(1/0)\t:H/L  0/I E/D  E/L  B/S   L/H   S/C   S/C\n");
 
-	for (i = 0; i < 16; i++)
+	for (i = 0; i < MAX_BLACKFIN_GPIOS; i++)
 		p += sprintf(p,
 			     "PF%d\t: %d....%d....%d....%d....%d....%d.....%d.....%d \n",
-			     i, ((data >> i) & 1), ((dir >> i) & 1),
-			     ((inen >> i) & 1), ((edge >> i) & 1),
-			     ((both >> i) & 1), ((polar >> i) & 1),
-			     ((maska >> i) & 1), ((maskb >> i) & 1));
-
+			     i, get_gpio_data(i), get_gpio_dir(i),
+			     get_gpio_inen(i), get_gpio_edge(i),
+			     get_gpio_both(i), get_gpio_polar(i),
+			     get_gpio_maska(i), get_gpio_maskb(i));
 	return p - buf;
 }
 
@@ -397,62 +323,37 @@ static int pflags_ioctl(struct inode *inode, struct file *filp, uint cmd,
 	if (minor < 0)
 		return -ENODEV;
 
-	DPRINTK("pfbits driver for bf53x minor = %d\n", minor);
+	pr_debug("%s: minor = %d\n",__FUNCTION__, minor);
 
 	switch (cmd) {
 	case SET_FIO_DIR:
 		{
-			DPRINTK("pflags_ioctl: SET_FIO_DIR\n");
-
-			if (arg)	// OUTPUT
-				bfin_write_FIO_DIR(bfin_read_FIO_DIR() | (1 << minor));
-			else	// INPUT
-				bfin_write_FIO_DIR(bfin_read_FIO_DIR() & ~(1 << minor));
-
+			pr_debug("%s: SET_FIO_DIR arg = %d\n",__FUNCTION__, (int)arg);
+			set_gpio_dir(minor, arg);
 			break;
 		}
 	case SET_FIO_POLAR:
 		{
-			DPRINTK("pflags_ioctl: SET_FIO_POLAR \n", arg);
-
-			if (arg)	// ACTIVELOW_FALLINGEDGE
-				bfin_write_FIO_POLAR(bfin_read_FIO_POLAR() | (1 << minor));
-			else	// ACTIVEHIGH_RISINGEDGE
-				bfin_write_FIO_POLAR(bfin_read_FIO_POLAR() & ~(1 << minor));
-
+			pr_debug("%s: SET_FIO_POLAR arg = %d\n",__FUNCTION__, (int)arg);
+			set_gpio_polar(minor, arg);
 			break;
 		}
 	case SET_FIO_EDGE:
 		{
-			DPRINTK("pflags_ioctl: SET_FIO_EDGE\n");
-
-			if (arg)	// EDGE
-				bfin_write_FIO_EDGE(bfin_read_FIO_EDGE() | (1 << minor));
-			else	// LEVEL
-				bfin_write_FIO_EDGE(bfin_read_FIO_EDGE() & ~(1 << minor));
-
+			pr_debug("%s: SET_FIO_EDGE arg = %d\n",__FUNCTION__, (int)arg);
+			set_gpio_edge(minor, arg);
 			break;
 		}
 	case SET_FIO_BOTH:
 		{
-			DPRINTK("pflags_ioctl: SET_FIO_BOTH\n");
-
-			if (arg)	// BOTHEDGES
-				bfin_write_FIO_BOTH(bfin_read_FIO_BOTH() | (1 << minor));
-			else	// SINGLEEDGE
-				bfin_write_FIO_BOTH(bfin_read_FIO_BOTH() & ~(1 << minor));
-
+			pr_debug("%s: SET_FIO_BOTH arg = %d\n",__FUNCTION__, (int)arg);
+			set_gpio_both(minor, arg);
 			break;
 		}
 	case SET_FIO_INEN:
 		{
-			DPRINTK("pflags_ioctl: SET_FIO_INEN\n");
-
-			if (arg)	// OUTPUT_ENABLE
-				bfin_write_FIO_INEN(bfin_read_FIO_INEN() | (1 << minor));
-			else	// INPUT_DISABLE
-				bfin_write_FIO_INEN(bfin_read_FIO_INEN() & ~(1 << minor));
-
+			pr_debug("%s: SET_FIO_INEN arg = %d\n",__FUNCTION__, (int)arg);
+			set_gpio_inen(minor, arg);
 			break;
 		}
 	default:
@@ -469,9 +370,6 @@ static struct file_operations pflags_fops = {
       .ioctl   = pflags_ioctl,
       .open    = pflags_open,
       .release = pflags_release,
-#ifdef ENABLE_POLL
-      .poll    = pflags_poll
-#endif
 };
 
 static int __init blackfin_pflags_init(void)
@@ -480,23 +378,7 @@ static int __init blackfin_pflags_init(void)
 
 	create_proc_read_entry("driver/pflags", 0, 0, pflags_read_proc, NULL);
 
-	/* FIXME: Remove following two lines as soon the default config has changed in u-boot */
-	bfin_write_FIO_MASKA_C((PF8 | PF6 | PF5));
-	bfin_write_FIO_EDGE(bfin_read_FIO_EDGE() & ~(PF8 | PF6 | PF5));
-
-#ifdef ENABLE_POLL
-	if (request_irq(IRQ_PROG_INTA, pflags_irq_handler, SA_INTERRUPT, "pflags", NULL)) {
-		printk(KERN_WARNING "pflags: IRQ %d is not free.\n", IRQ_PROG_INTA);
-		return -EIO;
-	}
-	init_waitqueue_head(&pflags_in_waitq);
-	pflags_laststate = bfin_read_FIO_FLAG_D();
-	pflags_statechanged = 0xffff;
-	printk(KERN_INFO "pfx: pfbits driver for bf53x IRQ %d\n", IRQ_PROG_INTA);
-#else
-	printk(KERN_INFO "pfx: pfbits driver for bf53x\n");
-#endif
-//	enable_irq(IRQ_PROG_INTA);
+	printk(KERN_INFO "pfx: pfbits driver for bf5xx\n");
 
 	return 0;
 }
@@ -504,6 +386,7 @@ static int __init blackfin_pflags_init(void)
 void __exit blackfin_plags_exit(void)
 {
 	remove_proc_entry("driver/pflags", NULL);
+	unregister_chrdev(PFLAG_MAJOR, "pflag");
 }
 
 module_init(blackfin_pflags_init);
