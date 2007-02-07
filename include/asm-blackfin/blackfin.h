2f7211bb890ec7e0996ce69b9616292a0e34be11
Michael Hennerich <michael.hennerich@analog.com>
2007-02-07 13:36:58 +0000

    [#2424] Cleanup - no functional change
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@2740 526b6c2d-f592-4532-a319-5dd88ccb003d

--- include/asm-blackfin/blackfin.h
+++ include/asm-blackfin/blackfin.h
@@ -18,24 +18,19 @@
                                             "sti %0;\n\t" \
                                              :"=d"(_tmp):);\
  } while (0)
-#endif 
-
-#if defined(ANOMALY_05000312) && !defined(ANOMALY_05000244) 
+#elif defined(ANOMALY_05000312) && !defined(ANOMALY_05000244) 
 #define SSYNC() do { 	int _tmp; \
                        __asm__ __volatile__ ("cli %0;\n\t"\
                                             "ssync;\n\t"\
                                             "sti %0;\n\t" \
                                              :"=d"(_tmp):);\
  } while (0)
-#endif 
-
-#if !defined(ANOMALY_05000312) && defined(ANOMALY_05000244) 
+#elif !defined(ANOMALY_05000312) && defined(ANOMALY_05000244) 
 #define SSYNC() do {__builtin_bfin_ssync();} while (0)	
-#endif
-
-#if !defined(ANOMALY_05000312) && !defined(ANOMALY_05000244) 
+#elif !defined(ANOMALY_05000312) && !defined(ANOMALY_05000244) 
 #define SSYNC() do {__asm__ __volatile__ ("ssync;\n\t") } while (0)
 #endif
+
 
 #if defined(ANOMALY_05000312) && defined(ANOMALY_05000244) 
 #define CSYNC() do { 	int _tmp; \
@@ -45,22 +40,16 @@
                                             "sti %0;\n\t" \
                                              :"=d"(_tmp):);\
  } while (0)
-#endif 
-
-#if defined(ANOMALY_05000312) && !defined(ANOMALY_05000244) 
+#elif defined(ANOMALY_05000312) && !defined(ANOMALY_05000244) 
 #define CSYNC() do { 	int _tmp; \
                        __asm__ __volatile__ ("cli %0;\n\t"\
                                             "csync;\n\t"\
                                             "sti %0;\n\t" \
                                              :"=d"(_tmp):);\
  } while (0)
-#endif 
-
-#if !defined(ANOMALY_05000312) && defined(ANOMALY_05000244) 
+#elif !defined(ANOMALY_05000312) && defined(ANOMALY_05000244) 
 #define CSYNC() do {__builtin_bfin_csync();} while (0)	
-#endif
-
-#if !defined(ANOMALY_05000312) && !defined(ANOMALY_05000244) 
+#elif !defined(ANOMALY_05000312) && !defined(ANOMALY_05000244) 
 #define CSYNC() do {__asm__ __volatile__ ("csync;\n\t") } while (0)
 #endif
 
