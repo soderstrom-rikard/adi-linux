2c8e5427a139a643e9ae9190e375c0528f947c14
Bryan Wu <cooloney@kernel.org>
2007-03-23 08:23:58 +0000

    [#2884] [LKML] Fixup the code in blackfin cache header file
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@2926 526b6c2d-f592-4532-a319-5dd88ccb003d

--- include/asm-blackfin/cache.h
+++ include/asm-blackfin/cache.h
@@ -1,13 +1,21 @@
+/*
+ * include/asm-blackfin/cache.h
+ */
 #ifndef __ARCH_BLACKFIN_CACHE_H
 #define __ARCH_BLACKFIN_CACHE_H
 
-/* bytes per L1 cache line */
-#define        L1_CACHE_SHIFT  5	/* BlackFin loads 32 bytes for cache */
-#define        L1_CACHE_BYTES  (1 << L1_CACHE_SHIFT)
+/* 
+ * Bytes per L1 cache line 
+ * Blackfin loads 32 bytes for cache
+ */
+#define L1_CACHE_SHIFT	5
+#define L1_CACHE_BYTES	(1 << L1_CACHE_SHIFT)
 
-/* For speed we do need to align these ...MaTed---*/
-/*  But include/linux/cache.h does this for us if we DO not define ...MaTed---*/
-#define __cacheline_aligned	/***** maybe no need this   Tony *****/
+/* 
+ * Don't make __cacheline_aligned and
+ * ____cacheline_aligned defined in include/linux/cache.h
+ */
+#define __cacheline_aligned
 #define ____cacheline_aligned
 
 /*
