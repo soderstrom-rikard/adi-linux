5f2ab54c767197277d5df34b2318ff745c48f0a4
Mike Frysinger <vapier@gentoo.org>
2007-05-25 15:57:56 +0000

    punt svn:keywords property and add svn:executable where appropriate
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@3201 526b6c2d-f592-4532-a319-5dd88ccb003d

--- scripts/mod/sumversion.c
+++ scripts/mod/sumversion.c
@@ -468,7 +468,7 @@ static int strip_rcs_crap(char *version)
 	full_len = strlen(version) + strlen(version + strlen(version) + 1) + 2;
 
 	/* Move string to start with version number: prefix will be
-	 * $Revision$ or $Revision: */
+	 * $Revision: 3155 $ or $Revision: */
 	len = strlen("$Revision");
 	if (version[len] == ':' || version[len] == '$')
 		len++;
