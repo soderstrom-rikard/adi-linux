7a6a989668bcd89066dc3a77cc204816781a3e11
Roy Huang <roy.huang@analog.com>
2007-06-07 00:13:46 +0000

    Revert previous changes to gpio.h. Task[#3217]
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@3273 526b6c2d-f592-4532-a319-5dd88ccb003d

--- include/asm-blackfin/gpio.h
+++ include/asm-blackfin/gpio.h
@@ -83,8 +83,8 @@
 #ifndef __ARCH_BLACKFIN_GPIO_H__
 #define __ARCH_BLACKFIN_GPIO_H__
 
-#define gpio_bank(x) ((x + 15) >> 4)
-#define gpio_bit(x)  (1<<((x % 16) & 0xF))
+#define gpio_bank(x) ((x) >> 4)
+#define gpio_bit(x)  (1<<(x & 0xF))
 #define gpio_sub_n(x) ((x) & 0xF)
 
 #define GPIO_BANKSIZE 16
