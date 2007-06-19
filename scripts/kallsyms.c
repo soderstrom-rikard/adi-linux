ae9333231d03af60a677fdb64423b05e56705201
Mike Frysinger <vapier@gentoo.org>
2007-06-19 17:13:21 +0000

    [#3283] snipe from -mm tree for OS X users:
    [PATCH] remove usage of memmem from scripts/kallsyms.c
    The only in-kernel user of "memmem" is scripts/kallsyms.c and it only
    uses it to find tokens that are 2 bytes in size. It is trivial to
    replace it with a simple function that finds 2-byte tokens.
    
    This should help users from systems that don't have the memmem GNU
    extension available.
    
    Signed-off-by: Paulo Marques <pmarques@grupopie.com>
    Acked-by: Mike Frysinger <vapier@gentoo.org>
    
    
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@3320 526b6c2d-f592-4532-a319-5dd88ccb003d

--- scripts/kallsyms.c
+++ scripts/kallsyms.c
@@ -386,6 +386,17 @@ static void build_initial_tok_table(void)
 	table_cnt = pos;
 }
 
+static void *find_token(unsigned char *str, int len, unsigned char *token)
+{
+	int i;
+
+	for (i = 0; i < len - 1; i++) {
+		if (str[i] == token[0] && str[i+1] == token[1])
+			return &str[i];
+	}
+	return NULL;
+}
+
 /* replace a given token in all the valid symbols. Use the sampled symbols
  * to update the counts */
 static void compress_symbols(unsigned char *str, int idx)
@@ -399,7 +410,7 @@ static void compress_symbols(unsigned char *str, int idx)
 		p1 = table[i].sym;
 
 		/* find the token on the symbol */
-		p2 = memmem(p1, len, str, 2);
+		p2 = find_token(p1, len, str);
 		if (!p2) continue;
 
 		/* decrease the counts for this symbol's tokens */
@@ -418,7 +429,7 @@ static void compress_symbols(unsigned char *str, int idx)
 			if (size < 2) break;
 
 			/* find the token on the symbol */
-			p2 = memmem(p1, size, str, 2);
+			p2 = find_token(p1, size, str);
 
 		} while (p2);
 
