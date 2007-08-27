c80bc22e8f3ec819aae6a8ab3ae7321cf56083f7
Michael Hennerich <michael.hennerich@analog.com>
2007-08-27 12:06:58 +0000

    [#2594] a few things still use bfin_read_PORT_FER()
    Add support for peripheral request API 
    
    [#2424] Cleanup
    ...
    
    
    
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@3619 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/mmc/spi_mmc/bfin_pio_spi.c
+++ drivers/mmc/spi_mmc/bfin_pio_spi.c
@@ -1,19 +1,18 @@
 #include <linux/kernel.h>	/* printk() */
 #include <linux/delay.h>
+#include <linux/errno.h>
+#include <asm/blackfin.h>
+#include <asm/gpio.h>
+#include <asm/portmux.h>
 
 #define SPI0_REGBASE       0xffc00500
-
-static volatile unsigned short *pFIO_FLAG_S = (volatile unsigned short*)0xFFC00708;
-static volatile unsigned short *pFIO_FLAG_C = (volatile unsigned short*)0xFFC00704;
-
-#define ssync() __builtin_bfin_ssync()
+#define DRV_NAME "mmc_pio_spi"
 
 #define DEFINE_SPI_REG(reg, off) \
 static inline u16 read_##reg(void) \
-            { return *(volatile unsigned short*)(SPI0_REGBASE + off); } \
+	{ return bfin_read16(SPI0_REGBASE + off); } \
 static inline void write_##reg(u16 v) \
-            {*(volatile unsigned short*)(SPI0_REGBASE + off) = v;\
-             ssync();}
+	{bfin_write16(SPI0_REGBASE + off, v); }
 
 #define POLL_TIMEOUT 1000000000
 
@@ -25,52 +24,51 @@ DEFINE_SPI_REG(RDBR, 0x10)
 DEFINE_SPI_REG(BAUD, 0x14)
 DEFINE_SPI_REG(SHAW, 0x18)
 
-static int cs_unselect_delay_ns = 0;
-static unsigned short spi_mmc_cs_chan_mask = 0;
+static int cs_unselect_delay_ns;
+static unsigned short spi_ssel;
 
 void bfin_pio_spi_set_hz(unsigned int hz)
 {
-    u_long sclk = get_sclk();
-    unsigned short baud = (sclk / (2 * hz));
+	u_long sclk = get_sclk();
+	unsigned short baud = (sclk / (2 * hz));
 
-        if ((sclk % (2 * hz)) > 0)
-                baud++;
+	if ((sclk % (2 * hz)) > 0)
+		baud++;
 
 	write_BAUD(baud);
-	ssync();
+	SSYNC();
 }
 
-void bfin_pio_spi_select( void )
+void bfin_pio_spi_select(void)
 {
 	write_STAT(0xFFFF);
 	write_CTRL(0x5009);
-	ssync();
+	SSYNC();
 
-	*pFIO_FLAG_C = spi_mmc_cs_chan_mask;
-	
+	gpio_set_value(spi_ssel, 0);
+
 	return;
 }
 
-void bfin_pio_spi_unselect( void )
+void bfin_pio_spi_unselect(void)
 {
 	// cs_unselect_delay_ns is filescoped and set to 0 when not accessing MACs
-	if(cs_unselect_delay_ns>0)
-        ndelay(cs_unselect_delay_ns);
+	if (cs_unselect_delay_ns > 0)
+		ndelay(cs_unselect_delay_ns);
 
-	*pFIO_FLAG_S = spi_mmc_cs_chan_mask;
-
-	ssync();
+	gpio_set_value(spi_ssel, 1);
+	SSYNC();
 
 	return;
 }
 
-unsigned int bfin_pio_spi_transfer(unsigned char* in, unsigned char out)
+unsigned int bfin_pio_spi_transfer(unsigned char *in, unsigned char out)
 {
 	/*
-	static unsigned char sr;
-	static unsigned char sd;
-	*/
-	unsigned int tc=POLL_TIMEOUT;
+	   static unsigned char sr;
+	   static unsigned char sd;
+	 */
+	unsigned int tc = POLL_TIMEOUT;
 	unsigned short SPI;
 
 	// Transmit data.
@@ -79,20 +77,18 @@ unsigned int bfin_pio_spi_transfer(unsigned char* in, unsigned char out)
 	// Wait until d is transmitted.
 	SPI = read_STAT();
 
-	while((SPI & 0x0008) && tc--)
-	{ 
+	while ((SPI & 0x0008) && tc--) {
 		SPI = read_STAT();
 	}
 
 	// Wait until recv buffer is ready.
-	tc=POLL_TIMEOUT;
-	while( !(SPI & 0x20) && tc-- )
-	{ 
+	tc = POLL_TIMEOUT;
+	while (!(SPI & 0x20) && tc--) {
 		SPI = read_STAT();
 	}
-	if(!tc) {
+	if (!tc) {
 		return 0;
-    }
+	}
 	// Read out recv buffer.
 	*in = read_RDBR();
 
@@ -102,50 +98,35 @@ unsigned int bfin_pio_spi_transfer(unsigned char* in, unsigned char out)
 
 void print_spi_debug_info(void)
 {
-    printk("cs_mask: 0x%x, baud=0x%x, flag=0x%x, ctrl=0x%x, mux= 0x%x, fer=0x%x, dir=0x%x\n",
-                spi_mmc_cs_chan_mask,
-                read_BAUD(),
-                read_FLAG(),
-                read_CTRL(),
-                bfin_read_PORT_MUX(),
-                bfin_read_PORTF_FER(),
-                bfin_read_FIO_DIR());
-
-    while(0);
+	printk
+	    (KERN_INFO DRV_NAME":cs_#: 0x%x, baud=0x%x, flag=0x%x, ctrl=0x%x\n",
+	     spi_ssel, read_BAUD(), read_FLAG(), read_CTRL());
 }
 
 // bdrate=0x100, flag=0xBF40, control_reg=0x1002, PORTF_FER=0x385F, PORT_MUX=0xA0
 void bfin_pio_spi_init(int cs_chan)
 {
-	spi_mmc_cs_chan_mask = 1 << cs_chan;
-    
- //   unsigned short baud, flag, ctrl, fer, mux;
 
-    print_spi_debug_info();
+	u16 pin_req[] = { P_SPI0_SCK, P_SPI0_MISO, P_SPI0_MOSI, 0 };
+	spi_ssel = (u16) cs_chan;
+
+	print_spi_debug_info();
 
 	// Set FLAG to 0xFF00 to drive CS manually using GPIOS
-	write_FLAG(0xFF00);	
-    write_CTRL(0x5009);
+	write_FLAG(0xFF00);
+	write_CTRL(0x5009);
 
-#if defined(CONFIG_BF534)|defined(CONFIG_BF536)|defined(CONFIG_BF537)
-    // Enable MOSI,MISO and SCLK
-    bfin_write_PORTF_FER(bfin_read_PORTF_FER() | 1 << 11); 
-    bfin_write_PORTF_FER(bfin_read_PORTF_FER() | 1 << 12); 
-    bfin_write_PORTF_FER(bfin_read_PORTF_FER() | 1 << 13);
+	if (peripheral_request_list(pin_req, DRV_NAME))
+		printk(KERN_ERR DRV_NAME":Requesting Peripherals failed\n");
 
-    // Enable correct PFx pin as chip select signal
-	bfin_write_PORTF_FER(bfin_read_PORTF_FER() & ~spi_mmc_cs_chan_mask);
-	SSYNC();
-#else
-	// enable PFx as GPIO
-	bfin_write_PORT_FER(bfin_read_PORT_FER() & ~spi_mmc_cs_chan_mask);
-#endif
+	if (gpio_request(spi_ssel, DRV_NAME)) {
+		printk(KERN_ERR DRV_NAME":bfin_pio_spi: Failed ro request GPIO_%d\n",
+		       spi_ssel);
+	}
+	gpio_direction_output(spi_ssel);
 
-	// set PFx direction to OUTPUT
-	bfin_write_FIO_DIR(bfin_read_FIO_DIR() | spi_mmc_cs_chan_mask);
-	
 	write_BAUD(0x0100);
 
-	ssync();
-	bfin_pio_spi_unselect( ) ;
+	SSYNC();
+	bfin_pio_spi_unselect();
 }
