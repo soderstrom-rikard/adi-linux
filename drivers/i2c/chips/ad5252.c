9c5aa491cf73200b7ebe83619cba0035d5ea27e8
Michael Hennerich <michael.hennerich@analog.com>
2007-12-12 14:52:31 +0000

    [#2424] Fix warn_unused_result warning
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@4031 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/i2c/chips/ad5252.c
+++ drivers/i2c/chips/ad5252.c
@@ -184,10 +184,14 @@ int ad5252_detect(struct i2c_adapter *adapter, int address, int kind)
 	ad5252_init_client(new_client);
 
 	/* Register sysfs hooks */
-	device_create_file(&new_client->dev, &dev_attr_read_w1);
-	device_create_file(&new_client->dev, &dev_attr_read_w3);
-	device_create_file(&new_client->dev, &dev_attr_write_w1);
-	device_create_file(&new_client->dev, &dev_attr_write_w3);
+	err = evice_create_file(&new_client->dev, &dev_attr_read_w1);
+	err |= device_create_file(&new_client->dev, &dev_attr_read_w3);
+	err |= device_create_file(&new_client->dev, &dev_attr_write_w1);
+	err |= device_create_file(&new_client->dev, &dev_attr_write_w3);
+
+	if (err)
+		printk(KERN_WARNING "AD5252: failed to register sysfs hooks\n");
+
 	printk(KERN_INFO "AD5252 Attached\n");
 	return 0;
 
