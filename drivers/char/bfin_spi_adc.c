90bf9bd74779d31f150d81185f85da2ff02a2d50
Michael Hennerich <michael.hennerich@analog.com>
2008-02-08 10:48:40 +0000

    [#3476] Enable kernel generic spidev driver for blackfin SPI ADC This is just a spiadc driver cleanup, see comments in the tracker.
    -Move stuff that doesn't belong here into user space.
    -Cleanup leftovers form the adsp-spiadc driver.
    -Fix variable SPI CLK frequency.
    
    
    
    
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@4239 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/char/bfin_spi_adc.c
+++ drivers/char/bfin_spi_adc.c
@@ -6,10 +6,9 @@
  * Created:
  * Description:
  *
- * Rev:          $Id$
  *
  * Modified:
- *               Copyright 2004-2006 Analog Devices Inc.
+ *               Copyright 2004-2008 Analog Devices Inc.
  *
  * Bugs:         Enter bugs at http://blackfin.uclinux.org/
  *
@@ -43,7 +42,9 @@
 #include <linux/miscdevice.h>
 #include <linux/spi/spi.h>
 #include <linux/spinlock.h>
+#include <linux/dma-mapping.h>
 
+#include <asm/dma-mapping.h>
 #include <asm/io.h>
 #include <asm/irq.h>
 #include <asm/blackfin.h>
@@ -51,31 +52,15 @@
 #include <asm/bfin5xx_spi.h>
 #include <asm/dma.h>
 
-#define TOL 		       5
-
-#define TIMEOUT		       50
-
-#define SPI_ADC_MAJOR          252   /* experiential */
 #define SPI0_ADC_MINOR         251
-
-#define SPI_ADC_DEVNAME       "BFIN_SPI_ADC"
-#define SPI_ADC_INTNAME       "BFIN_SPIINT"  /* Should be less than 19 chars. */
 
 struct bfin_spi_adc {
 	int             opened;
-	int             timeout;
-	unsigned char   sense;
-	unsigned char   edge;
-	unsigned int    triggerpos;
-	unsigned int    actcount;
 	unsigned short  *buffer;
-	unsigned short  level;
-	unsigned char   mode;
-	unsigned char   cont;
-	unsigned short  skfs;
-	int             baud;
-
+	int             hz;
+	int 		cont;
 	struct spi_device *spidev;
+	dma_addr_t dma_handle;
 };
 
 struct bfin_spi_adc spi_adc;
@@ -87,30 +72,6 @@ static int adc_spi_ioctl(struct inode *inode, struct file *filp, uint cmd, unsig
 	struct bfin_spi_adc *bfin_spi_adc = filp->private_data;
 
 	switch (cmd) {
-	case CMD_SPI_SET_TRIGGER_MODE:
-	{
-		pr_debug("spi_ioctl: CMD_SPI_SET_TRIGGER_MODE\n");
-		bfin_spi_adc->mode = (unsigned char)arg;
-		break;
-	}
-	case CMD_SPI_SET_TRIGGER_SENSE:
-	{
-		pr_debug("spi_ioctl: CMD_SPI_SET_TRIGGER_SENSE\n");
-		bfin_spi_adc->sense = (unsigned char)arg;
-		break;
-	}
-	case CMD_SPI_SET_TRIGGER_EDGE:
-	{
-		pr_debug("spi_ioctl: CMD_SPI_SET_TRIGGER_EDGE\n");
-		bfin_spi_adc->edge = (unsigned char)arg;
-		break;
-	}
-	case CMD_SPI_SET_TRIGGER_LEVEL:
-	{
-		pr_debug("spi_ioctl: CMD_SPI_SET_TRIGGER_LEVEL\n");
-		bfin_spi_adc->level = (unsigned short)arg;
-		break;
-	}
 	case CMD_SPI_GET_SYSTEMCLOCK:
 	{
 		value = get_sclk();
@@ -119,24 +80,14 @@ static int adc_spi_ioctl(struct inode *inode, struct file *filp, uint cmd, unsig
 	}
 	case CMD_SPI_SET_BAUDRATE:
 	{
-		pr_debug("spi_ioctl: CMD_SPI_SET_BAUDRATE\n");
-		/* BaudRate 0,1 unavail */
-		if ((unsigned short)arg <= 1)
+		if (arg > (133000000 / 4))
 			return -EINVAL;
-		/* SPI's baud rate is SCLK / ( arg * 2) */
-		bfin_spi_adc->baud = (unsigned short)arg;
+		bfin_spi_adc->hz = arg;
 		break;
 	}
 	case CMD_SPI_SET_WRITECONTINUOUS:
 	{
-		pr_debug("spi_ioctl: CMD_SPI_SET_WRITECONTINUOUS\n");
 		bfin_spi_adc->cont = (unsigned char)arg;
-		break;
-	}
-	case CMD_SPI_SET_SKFS:
-	{
-		pr_debug("spi_ioctl: CMD_SPI_SET_WRITECONTINUOUS\n");
-		bfin_spi_adc->skfs = (unsigned short)arg;
 		break;
 	}
 	default:
@@ -147,155 +98,95 @@ static int adc_spi_ioctl(struct inode *inode, struct file *filp, uint cmd, unsig
 
 static ssize_t adc_spi_read (struct file *filp, char *buf, size_t count, loff_t *pos)
 {
-	struct spi_transfer t;
-	struct spi_message m;
-	int i;
-	int repeat_reading;
+	int stat;
 	struct bfin_spi_adc *bfin_spi_adc = filp->private_data;
-	repeat_reading = 0;
+	u8 *buffer;
+
+	struct spi_transfer	t = {
+			.len		= count,
+			.speed_hz	= bfin_spi_adc->hz,
+		};
+	struct spi_message	m;
 
 	if (count <= 0)
-		return 0;
+		return -EINVAL;
 
-	bfin_spi_adc->timeout = TIMEOUT;
+	buffer = dma_alloc_coherent(NULL, count, &bfin_spi_adc->dma_handle, GFP_KERNEL);
 
-	if (bfin_spi_adc->mode)
-		bfin_spi_adc->actcount = 2 * count;
-	else
-		bfin_spi_adc->actcount = count;
+	if (buffer == NULL)
+		return -ENOMEM;
 
-	/* Allocate some memory */
-	bfin_spi_adc->buffer = kmalloc(bfin_spi_adc->actcount + 2*bfin_spi_adc->skfs, GFP_KERNEL);
-
-	/* Invalidate allocated memory in Data Cache */
-	blackfin_dcache_invalidate_range((unsigned long)bfin_spi_adc->buffer,((unsigned long) bfin_spi_adc->buffer)+ bfin_spi_adc->actcount + 2*bfin_spi_adc->skfs);
+	t.rx_buf = buffer;
 
 	spi_message_init(&m);
-	memset(&t, 0, (sizeof t));
-	t.rx_buf = bfin_spi_adc->buffer;
-	t.len = bfin_spi_adc->actcount + (2*bfin_spi_adc->skfs);
+	spi_message_add_tail(&t, &m);
 
-	do {
-		spi_message_add_tail(&t, &m);
-		spi_sync(bfin_spi_adc->spidev, &m);
+	stat = spi_sync(bfin_spi_adc->spidev, &m);
 
-		bfin_spi_adc->triggerpos=0;
+	if (stat == 0) {
+		unsigned long	missing;
 
-		if (bfin_spi_adc->mode) {
-			/* Search for trigger condition */
-			if (bfin_spi_adc->sense) {
-				/* Edge sensitive */
-				if (bfin_spi_adc->edge){
-					/* Falling edge */
-					bfin_spi_adc->triggerpos=0;
-					for (i=1+bfin_spi_adc->skfs;(i < bfin_spi_adc->actcount)&& !bfin_spi_adc->triggerpos;i++) {
-						if ((bfin_spi_adc->buffer[i-1] > bfin_spi_adc->level)&&(bfin_spi_adc->buffer[i+1] < bfin_spi_adc->level)) {
-							bfin_spi_adc->triggerpos=i;
-							i=bfin_spi_adc->actcount;
-						}
-					}
-					if (!bfin_spi_adc->triggerpos && bfin_spi_adc->timeout--) repeat_reading = 1;
+		missing = copy_to_user(buf, buffer, count);
+		if (count && missing == count)
+			stat = -EFAULT;
+		else
+			stat = count - missing;
+	}
 
-				} else {
-					/* Rising edge */
-					bfin_spi_adc->triggerpos=0;
-					for (i=1+bfin_spi_adc->skfs;(i < bfin_spi_adc->actcount)&& !bfin_spi_adc->triggerpos;i++) {
+	dma_free_coherent(NULL, count, buffer, bfin_spi_adc->dma_handle);
 
-						if ((bfin_spi_adc->buffer[i-1] < bfin_spi_adc->level)&&(bfin_spi_adc->buffer[i+1] > bfin_spi_adc->level)) {
-							bfin_spi_adc->triggerpos=i;
-							i=bfin_spi_adc->actcount;
-						}
-					}
-
-					if (!bfin_spi_adc->triggerpos && bfin_spi_adc->timeout--) repeat_reading = 1;
-				}
-			} else {
-				if (bfin_spi_adc->edge){
-					/* Falling edge */
-					bfin_spi_adc->triggerpos=0;
-					for (i=1+bfin_spi_adc->skfs;(i < bfin_spi_adc->actcount)&& !bfin_spi_adc->triggerpos;i++) {
-						if ((bfin_spi_adc->buffer[i-1] > bfin_spi_adc->level)&&(bfin_spi_adc->buffer[i+1] < bfin_spi_adc->level)) {
-							bfin_spi_adc->triggerpos=i;
-							i=bfin_spi_adc->actcount;
-						}
-					}
-					if (!bfin_spi_adc->triggerpos && bfin_spi_adc->timeout--) repeat_reading = 1;
-				} else {
-					/* Rising edge */
-					bfin_spi_adc->triggerpos=0;
-					for (i=1+bfin_spi_adc->skfs;(i < bfin_spi_adc->actcount)&& !bfin_spi_adc->triggerpos;i++) {
-						if ((bfin_spi_adc->buffer[i-1] < bfin_spi_adc->level)&&(bfin_spi_adc->buffer[i+1] > bfin_spi_adc->level)) {
-							bfin_spi_adc->triggerpos=i;
-							i=bfin_spi_adc->actcount;
-						}
-					}
-
-					if (!bfin_spi_adc->triggerpos && bfin_spi_adc->timeout--) repeat_reading = 1;
-				}
-			}
-		}
-	} while (repeat_reading);
-
-
-	pr_debug(" triggerpos = %d \n",bfin_spi_adc->triggerpos);
-	pr_debug(" skfs = %d \n",(u32)bfin_spi_adc->skfs);
-	pr_debug(" count = %d \n",(u32)count);
-
-	if (!(bfin_spi_adc->timeout < 0) && (!bfin_spi_adc->triggerpos))
-		copy_to_user(buf, bfin_spi_adc->buffer + bfin_spi_adc->skfs, count);
-	else
-		copy_to_user(buf, bfin_spi_adc->buffer + bfin_spi_adc->triggerpos, count);
-
-	kfree(bfin_spi_adc->buffer);
-
-	pr_debug(" timeout = %d \n",bfin_spi_adc->timeout);
-	if (bfin_spi_adc->timeout < 0)
-		return SPI_ERR_TRIG;
-
-	return count;
+	return stat;
 }
 
 static ssize_t adc_spi_write (struct file *filp, const char *buf, size_t count, loff_t *f_pos)
 {
-	struct spi_transfer t;
-	struct spi_message m;
+	int stat;
+	unsigned long missing;
 	struct bfin_spi_adc *bfin_spi_adc = filp->private_data;
+	u8 *buffer;
 
-	pr_debug("spi_write:\n");
+	struct spi_transfer	t = {
+			.len		= count,
+			.speed_hz	= bfin_spi_adc->hz,
+		};
+	struct spi_message	m;
 
 	if (count <= 0)
-		return 0;
+		return -EINVAL;
 
-	bfin_spi_adc->actcount = count;
-	bfin_spi_adc->timeout = TIMEOUT;
+	buffer = dma_alloc_coherent(NULL, count, &bfin_spi_adc->dma_handle, GFP_KERNEL);
 
-	blackfin_dcache_flush_range((unsigned long)buf,((unsigned long)buf+(count)));
+	if (buffer == NULL)
+		return -ENOMEM;
 
-	spi_message_init(&m);
-	memset(&t, 0, (sizeof t));
+	t.tx_buf = buffer;
+
 	if (bfin_spi_adc->cont)  /* dirty hack for continuous DMA output mode */
 		t.tx_dma = 0xFFFF;
 
-	t.tx_buf = buf;
-	t.len = count<<1;
-	pr_debug("in spi_adc driver, t.tx_buf is 0x%x,t.tx_dma is 0x%x, t.rx_buf is 0x%x,len is %d\n",(u32)t.tx_buf,(u32)t.tx_dma,(u32)t.rx_buf,(u32)t.len);
-
+	spi_message_init(&m);
 	spi_message_add_tail(&t, &m);
-	spi_sync(bfin_spi_adc->spidev, &m);
 
-	pr_debug("spi_write: return\n");
-	return count;
+	missing = copy_from_user(buffer, buf, count);
+
+	if (missing == 0) {
+		stat = spi_sync(bfin_spi_adc->spidev, &m);
+		if (stat == 0)
+			stat = count;
+	} else
+		stat = -EFAULT;
+
+	dma_free_coherent(NULL, count, buffer, bfin_spi_adc->dma_handle);
+
+	return stat;
 }
 
 static int adc_spi_open(struct inode *inode, struct file *filp)
 {
-	struct spi_device *spi;
 	unsigned long flags;
 	int minor = MINOR (inode->i_rdev);
 
-	pr_debug("spi_open: start\n");
 
-	/* SPI ? */
 	if (minor != SPI0_ADC_MINOR)
 		return -ENXIO;
 
@@ -305,27 +196,25 @@ static int adc_spi_open(struct inode *inode, struct file *filp)
 		return -EMFILE;
 	}
 
-	spi = spi_adc.spidev;
-	/* Clear configuration information */
-	memset(&spi_adc, 0, sizeof(struct bfin_spi_adc));
-
 	spi_adc.opened = 1;
-	spi_adc.cont = 0;
-	spi_adc.skfs = 0;
-	spi_adc.spidev = spi;
 
 	filp->private_data = &spi_adc;
 	spin_unlock_irqrestore(&spiadc_lock, flags);
 
-	pr_debug("spi_open: return\n");
 	return 0;
 }
 
 static int adc_spi_release (struct inode *inode, struct file *filp)
 {
-	spi_adc.opened = 0;
+	unsigned long flags;
+	struct bfin_spi_adc *bfin_spi_adc = filp->private_data;
 
-	pr_debug("spi_release: close() return\n");
+	spin_lock_irqsave(&spiadc_lock, flags);
+	bfin_spi_adc->opened = 0;
+	bfin_spi_adc->hz = 0;
+	bfin_spi_adc->cont = 0;
+	spin_unlock_irqrestore(&spiadc_lock, flags);
+
 	return 0;
 }
 
@@ -347,7 +236,7 @@ static int __devinit bfin_spi_adc_probe(struct spi_device *spi)
 
 static int __devexit bfin_spi_adc_remove(struct spi_device *spi)
 {
-	printk(KERN_ALERT "Goodbye SPI\n");
+	printk(KERN_INFO "Goodbye SPI\n");
 	return 0;
 }
 
@@ -372,7 +261,7 @@ static int __init bfin_spi_adc_init(void)
 	int result;
 	result = misc_register(&bfin_adc_dev);
 	if (result < 0) {
-		printk(KERN_WARNING "SPI: can't get minor %d\n", SPI_ADC_MAJOR);
+		printk(KERN_WARNING "SPI: Failed to register\n");
 		return result;
 	}
 
