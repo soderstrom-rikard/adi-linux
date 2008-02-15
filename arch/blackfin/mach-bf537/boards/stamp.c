f77cb00ca45d5b248be84b590d8374cb5dc890df
Mingquan Pan <grace.pan@analog.com>
2008-02-15 04:12:14 +0000

    [#2443] Update u-boot partition size.
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@4266 526b6c2d-f592-4532-a319-5dd88ccb003d

--- arch/blackfin/mach-bf537/boards/stamp.c
+++ arch/blackfin/mach-bf537/boards/stamp.c
@@ -404,7 +404,7 @@ static struct platform_device stamp_flash_device = {
 static struct mtd_partition bfin_spi_flash_partitions[] = {
 	{
 		.name = "bootloader",
-		.size = 0x00020000,
+		.size = 0x00040000,
 		.offset = 0,
 		.mask_flags = MTD_CAP_ROM
 	}, {
