4e5eddabd09369fa7f07869344fa4c6903e2b673
Sonic Zhang <sonic.zhang@analog.com>
2008-02-27 10:26:12 +0000

    Bug[#3910] As pointed out, set timer delay to HZ/50.
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@4368 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/serial/bfin_5xx.c
+++ drivers/serial/bfin_5xx.c
@@ -48,7 +48,7 @@
 #define DMA_RX_XCOUNT		512
 #define DMA_RX_YCOUNT		(PAGE_SIZE / DMA_RX_XCOUNT)
 
-#define DMA_RX_FLUSH_JIFFIES	5
+#define DMA_RX_FLUSH_JIFFIES	(HZ / 50)
 
 #ifdef CONFIG_SERIAL_BFIN_DMA
 static void bfin_serial_dma_tx_chars(struct bfin_serial_port *uart);
