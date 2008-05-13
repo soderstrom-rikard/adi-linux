bbcbb6fafb6fa8054b8e936b130ea6c0bd3776bb
Mike Frysinger <vapier@gentoo.org>
2008-05-13 17:43:54 +0000

    slight backport from newer kernels to make driver posting easier on bryan
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@4729 526b6c2d-f592-4532-a319-5dd88ccb003d

--- include/linux/init.h
+++ include/linux/init.h
@@ -42,6 +42,7 @@
    discard it in modules) */
 #define __init		__attribute__ ((__section__ (".init.text"))) __cold
 #define __initdata	__attribute__ ((__section__ (".init.data")))
+#define __initconst	__attribute__ ((__section__ (".init.rodata")))
 #define __exitdata	__attribute__ ((__section__(".exit.data")))
 #define __exit_call	__attribute_used__ __attribute__ ((__section__ (".exitcall.exit")))
 
@@ -257,11 +258,13 @@ void __init parse_early_param(void);
 #ifdef CONFIG_HOTPLUG
 #define __devinit
 #define __devinitdata
+#define __devinitconst
 #define __devexit
 #define __devexitdata
 #else
 #define __devinit __init
 #define __devinitdata __initdata
+#define __devinitconst __initconst
 #define __devexit __exit
 #define __devexitdata __exitdata
 #endif
