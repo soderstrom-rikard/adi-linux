ccbf707b38932543bf96a74df4eb7bce4d9e67b9
Michael Hennerich <michael.hennerich@analog.com>
2008-08-29 14:48:20 +0000

    [#4373] BF54x / BF52x Rotary Input device driver Add some missing counter bit fields. Add counter portmux support Add Rotary BF52x platform device support
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@5224 526b6c2d-f592-4532-a319-5dd88ccb003d

--- include/asm-blackfin/bfin_rotary.h
+++ include/asm-blackfin/bfin_rotary.h
@@ -1,16 +1,16 @@
 #ifndef _BFIN_ROTARY_H
 #define _BFIN_ROTARY_H
 
-#define ROT_QUAD_ENC	0x0	/* quadrature / grey code encoder mode */
-#define ROT_BIN_ENC	0x100	/* binary encoder mode */
-#define ROT_UD_CNT	0x200	/* rotary counter mode */
-#define ROT_DIR_CNT	0x400	/* direction counter mode */
+#define ROT_QUAD_ENC	CNTMODE_QUADENC	/* quadrature / grey code encoder mode */
+#define ROT_BIN_ENC	CNTMODE_BINENC	/* binary encoder mode */
+#define ROT_UD_CNT	CNTMODE_UDCNT	/* rotary counter mode */
+#define ROT_DIR_CNT	CNTMODE_DIRCNT	/* direction counter mode */
 
-#define ROT_DEBE	0x2	/* Debounce Enable */
+#define ROT_DEBE	DEBE	/* Debounce Enable */
 
-#define ROT_CDGINV	0x10	/* CDG Pin Polarity Invert */
-#define ROT_CUDINV	0x20	/* CUD Pin Polarity Invert */
-#define ROT_CZMINV	0x40	/* CZM Pin Polarity Invert */
+#define ROT_CDGINV	CDGINV	/* CDG Pin Polarity Invert */
+#define ROT_CUDINV	CUDINV	/* CUD Pin Polarity Invert */
+#define ROT_CZMINV	CZMINV	/* CZM Pin Polarity Invert */
 
 struct bfin_rotary_platform_data {
 	unsigned int rotary_up_key;
