8b7b6f137c6ac81398fb5322b4240afbd442a419
Robin Getz <robin.getz@analog.com>
2008-09-15 21:56:46 +0000

    Make sure we program the correct values in only when necessary.
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@5293 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/usb/musb/blackfin.c
+++ drivers/usb/musb/blackfin.c
@@ -336,13 +336,15 @@ int __init musb_platform_init(struct musb *musb)
 	}
 	gpio_direction_output(GPIO_USB_VRSEL, 0);
 
-	/* Anomaly #05000346 */
-	bfin_write_USB_APHY_CALIB(0x5411);
-	SSYNC();
+	if (ANOMALY_05000346) {
+		bfin_write_USB_APHY_CALIB(ANOMALY_05000346_value);
+		SSYNC();
+	}
 
-	/* Anomaly #05000347 */
-	bfin_write_USB_APHY_CNTRL(0x0);
-	SSYNC();
+	if (ANOMALY_05000347) {
+		bfin_write_USB_APHY_CNTRL(0x0);
+		SSYNC();
+	}
 
 	/* TODO
 	 * Set SIC-IVG register
