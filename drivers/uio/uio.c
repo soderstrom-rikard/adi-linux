c14cddc0c79816126819381cac0e7bed9482e066
Mike Frysinger <vapier@gentoo.org>
2008-10-29 16:27:42 +0000

    only call pgprot_noncached if it is defined
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@5516 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/uio/uio.c
+++ drivers/uio/uio.c
@@ -529,7 +529,9 @@ static int uio_mmap_physical(struct vm_area_struct *vma)
 
 	vma->vm_flags |= VM_IO | VM_RESERVED;
 
+#ifdef pgprot_noncached
 	vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
+#endif
 
 	return remap_pfn_range(vma,
 			       vma->vm_start,
