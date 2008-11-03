a732e58189990fc846d454ab732dac83359704a9
Michael Hennerich <michael.hennerich@analog.com>
2008-11-03 12:04:20 +0000

    [#4588] bf537-lq035.c:check_fb() needs updating to 2.6.28
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@5567 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/video/bf537-lq035.c
+++ drivers/video/bf537-lq035.c
@@ -742,7 +742,7 @@ static int bfin_lcd_set_contrast(struct lcd_device* dev, int contrast)
 	return 0;
 }
 
-static int bfin_lcd_check_fb(struct fb_info* fi)
+static int bfin_lcd_check_fb(struct lcd_device *lcd, struct fb_info *fi)
 {
 	if (!fi || (fi == &bfin_lq035_fb))
 		return 1;
