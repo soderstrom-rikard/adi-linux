6f43d37504d458c53db7d260c280b2f896665ae4
Graf Yang <graf.yang@analog.com>
2008-11-24 07:03:33 +0000

    Minor typo change.
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@5748 526b6c2d-f592-4532-a319-5dd88ccb003d

--- arch/blackfin/kernel/bfin_gpio.c
+++ arch/blackfin/kernel/bfin_gpio.c
@@ -1057,7 +1057,7 @@ int bfin_gpio_request(unsigned gpio, const char *label)
 		return -EBUSY;
 	}
 	if (unlikely(reserved_gpio_irq_map[gpio_bank(gpio)] & gpio_bit(gpio)))
-		printk(KERN_NOTICE "bfin-gpio: GPIO %d is already reserved as gpio-irq, and you are making it as GPIO!\n", gpio);
+		printk(KERN_NOTICE "bfin-gpio: GPIO %d is already reserved as gpio-irq, and you are configuring it as GPIO!\n", gpio);
 
 	reserved_gpio_map[gpio_bank(gpio)] |= gpio_bit(gpio);
 	set_label(gpio, label);
@@ -1120,7 +1120,7 @@ int bfin_gpio_irq_request(unsigned gpio, const char *label)
 		return -EBUSY;
 	}
 	if (unlikely(reserved_gpio_map[gpio_bank(gpio)] & gpio_bit(gpio)))
-		printk(KERN_NOTICE "bfin-gpio: GPIO %d is already reserved by %s, and you are making it as IRQ!\n", gpio, get_label(gpio));
+		printk(KERN_NOTICE "bfin-gpio: GPIO %d is already reserved by %s, and you are configuring it as IRQ!\n", gpio, get_label(gpio));
 
 	reserved_gpio_irq_map[gpio_bank(gpio)] |= gpio_bit(gpio);
 	set_label(gpio, label);
