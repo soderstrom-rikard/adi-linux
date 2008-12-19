069772db2c4048b06830144935842b4ac76cdcda
Cliff Cai <cliff.cai@analog.com>
2008-12-19 06:01:26 +0000

    task[#4212]correct worng previous commiting
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@5898 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/mmc/host/bfin_sdh.c
+++ drivers/mmc/host/bfin_sdh.c
@@ -345,7 +345,7 @@ static void sdh_set_ios(struct mmc_host *mmc, struct mmc_ios *ios)
 		cfg &= ~0x80;
 		cfg |= 0x40;
 		/* Enable 4 bit SDIO */
-		cfgi |= 0x04;
+		cfg |= 0x04;
 		bfin_write_SDH_CFG(cfg);
 		clk_ctl |= WIDE_BUS;
 	}
