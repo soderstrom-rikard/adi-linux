bc405555938f3a18addc9118255813bdfe5f6685
Graf Yang <graf.yang@analog.com>
2008-12-19 10:55:25 +0000

    Not do anonymous mmap when oprofiling flat format application, let oprofile daemon can find out the file path.
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@5904 526b6c2d-f592-4532-a319-5dd88ccb003d

--- fs/binfmt_flat.c
+++ fs/binfmt_flat.c
@@ -608,7 +608,11 @@ static int load_flat_file(struct linux_binprm * bprm,
 		len = text_len + data_len + extra + MAX_SHARED_LIBS * sizeof(unsigned long);
 		len = PAGE_ALIGN(len);
 		down_write(&current->mm->mmap_sem);
+#ifdef CONFIG_OPROFILE
+		textpos = do_mmap(bprm->file, 0, len,
+#else
 		textpos = do_mmap(0, 0, len,
+#endif
 				PROT_READ | PROT_EXEC | PROT_WRITE,
 				MAP_PRIVATE, 0);
 		up_write(&current->mm->mmap_sem);
