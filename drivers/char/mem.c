af470260631e581539b9c11cc86a23bc0467760b
Mike Frysinger <vapier@gentoo.org>
2009-01-08 18:05:20 +0000

    revert previous change ... we want len here, not &len
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@5976 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -281,7 +281,7 @@ static unsigned long get_unmapped_area_mem(struct file *file,
 					   unsigned long pgoff,
 					   unsigned long flags)
 {
-	if (!valid_mmap_phys_addr_range(pgoff, &len))
+	if (!valid_mmap_phys_addr_range(pgoff, len))
 		return (unsigned long) -EINVAL;
 	return pgoff << PAGE_SHIFT;
 }
