6a5f025844638d9c471972bc3008e3abcf12576b
Robin Getz <robin.getz@analog.com>
2009-01-09 15:37:03 +0000

    [#2424] Print out where the bootmode is coming from (for easier debugging).
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@5990 526b6c2d-f592-4532-a319-5dd88ccb003d

--- arch/blackfin/kernel/setup.c
+++ arch/blackfin/kernel/setup.c
@@ -824,6 +824,9 @@ void __init setup_arch(char **cmdline_p)
 	flash_probe();
 #endif
 
+	printk(KERN_INFO "Boot Mode: %i, Reset Vector: 0x%04x0000\n",
+		bfin_read_SYSCR() & 0xF, bfin_read_SIC_RVECT());
+
 	_bfin_swrst = bfin_read_SWRST();
 
 #ifdef CONFIG_DEBUG_DOUBLEFAULT_PRINT
