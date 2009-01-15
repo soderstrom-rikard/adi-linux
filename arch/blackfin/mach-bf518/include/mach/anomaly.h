a89cf7390c49f5b4357d61766c8a9215e1a8ded7
Mike Frysinger <vapier@gentoo.org>
2009-01-15 05:12:48 +0000

    revert previous anomaly change ... bf51x is not affected by anomaly 05000353
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@6021 526b6c2d-f592-4532-a319-5dd88ccb003d

--- arch/blackfin/mach-bf518/include/mach/anomaly.h
+++ arch/blackfin/mach-bf518/include/mach/anomaly.h
@@ -53,8 +53,6 @@
 #define ANOMALY_05000443 (1)
 /* Incorrect L1 Instruction Bank B Memory Map Location */
 #define ANOMALY_05000444 (1)
-/* bfrom_SysControl() Firmware Function Performs Improper System Reset */
-#define ANOMALY_05000353 (1)
 
 /* Anomalies that don't exist on this proc */
 #define ANOMALY_05000125 (0)
@@ -72,6 +70,7 @@
 #define ANOMALY_05000311 (0)
 #define ANOMALY_05000312 (0)
 #define ANOMALY_05000323 (0)
+#define ANOMALY_05000353 (0)
 #define ANOMALY_05000363 (0)
 #define ANOMALY_05000386 (0)
 #define ANOMALY_05000412 (0)
