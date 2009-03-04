b7b6b5fcf0a42ca308c0579f0f7932df3b7e1eca
Sonic Zhang <sonic.zhang@analog.com>
2009-03-04 02:41:08 +0000

    Correct the condition Macro.
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@6150 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/net/irda/bfin_sir.c
+++ drivers/net/irda/bfin_sir.c
@@ -16,9 +16,9 @@
 #define DMA_SIR_RX_FLUSH_JIFS  (HZ * 4 / 250)
 #endif
 
-# if defined ANOMALY_05000447
+#if ANOMALY_05000447
 static int max_rate = 57600;
-# else
+#else
 static int max_rate = 115200;
 #endif
 
