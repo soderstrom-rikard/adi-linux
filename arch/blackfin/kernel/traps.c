fc9d6a793f2299a8a7ac244661479d0959f97e7a
Robin Getz <robin.getz@analog.com>
2009-06-22 02:02:16 +0000

    Today - if the global interrupt enable bit (IPEND[4]) is set (interrupts disabled) our trap handling code goes into a infinite loop, since we need interrupts to be on to defer things to level5.
    Normal kernel code should not do this for any reason (except when
    handling interrupt context save/restore - which should not cause
    exceptions) - but there have been a few times (bugs) where this
    has happened, so this improvement ensures that was a infinite hang,
    actually is processed properly, and a kernel oops message is generated
    
    
    
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@6818 526b6c2d-f592-4532-a319-5dd88ccb003d

--- arch/blackfin/kernel/traps.c
+++ arch/blackfin/kernel/traps.c
@@ -1115,8 +1115,15 @@ void show_regs(struct pt_regs *fp)
 	verbose_printk(KERN_NOTICE "%s", linux_banner);
 
 	verbose_printk(KERN_NOTICE "\n" KERN_NOTICE "SEQUENCER STATUS:\t\t%s\n", print_tainted());
-	verbose_printk(KERN_NOTICE " SEQSTAT: %08lx  IPEND: %04lx  SYSCFG: %04lx\n",
-		(long)fp->seqstat, fp->ipend, fp->syscfg);
+	verbose_printk(KERN_NOTICE " SEQSTAT: %08lx  IPEND: %04lx  IMASK: %04lx  SYSCFG: %04lx\n",
+		(long)fp->seqstat, fp->ipend, cpu_pda[smp_processor_id()].ex_imask, fp->syscfg);
+	if (fp->ipend & EVT_IRPTEN_P)
+		verbose_printk(KERN_NOTICE "  Global Interrupts Disabled (IPEND[4])\n");
+	if (!(cpu_pda[smp_processor_id()].ex_imask & (EVT_IVG13 | EVT_IVG12 | EVT_IVG11 |
+			EVT_IVG10 | EVT_IVG9 | EVT_IVG8 | EVT_IVG7 | EVT_IVTMR)))
+		verbose_printk(KERN_NOTICE "  Peripheral interrupts masked off\n");
+	if (!(cpu_pda[smp_processor_id()].ex_imask & (EVT_IVG15 | EVT_IVG14)))
+		verbose_printk(KERN_NOTICE "  Kernel interrupts masked off\n");
 	if ((fp->seqstat & SEQSTAT_EXCAUSE) == VEC_HWERR) {
 		verbose_printk(KERN_NOTICE "  HWERRCAUSE: 0x%lx\n",
 			(fp->seqstat & SEQSTAT_HWERRCAUSE) >> 14);
