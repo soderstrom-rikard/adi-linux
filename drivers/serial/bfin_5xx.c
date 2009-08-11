e2aad18b1df868a057534c03de3a4de0c48f3a88
Graf Yang <graf.yang@analog.com>
2009-08-11 04:19:22 +0000

    Correct the negation logic as Mike pointed out
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@7147 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/serial/bfin_5xx.c
+++ drivers/serial/bfin_5xx.c
@@ -863,7 +863,7 @@ bfin_serial_set_termios(struct uart_port *port, struct ktermios *termios,
 	quot = uart_get_divisor(port, baud);
 
 	/* If discipline is not IRDA, apply ANOMALY_05000230 */
-	if (port->info && (port->info->port.tty->termios->c_line != N_IRDA))
+	if (!port->info || (port->info->port.tty->termios->c_line != N_IRDA))
 		quot -= ANOMALY_05000230;
 
 	spin_lock_irqsave(&uart->port.lock, flags);
