6fe4dad93d0bb3be2e5882daeed223767eaae417
Mike Frysinger <vapier@gentoo.org>
2009-08-13 18:38:47 +0000

    move MUSB anomaly error info into musb driver
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@7183 526b6c2d-f592-4532-a319-5dd88ccb003d

--- arch/blackfin/mach-common/arch_checks.c
+++ arch/blackfin/mach-common/arch_checks.c
@@ -78,13 +78,3 @@
 	 (!defined(CONFIG_BFIN_EXTMEM_DCACHEABLE) && defined(CONFIG_BFIN_L2_WRITEBACK)))
 # error You are exposing Anomaly 220 in this config, either config L2 as Write Through, or make External Memory WB.
 #endif
-
-/* The Mentor usb DMA engine on BF52x(silicon v0.0 and v0.1)seems
- * not work stably in host mode. This may be caused by anomaly 05000380.
- * After digging out the root cause anomaly, change its number  accordingly.
- * So,either using silicon v0.2 or heighter or disabling DMA mode in musb.
- */
-#if ANOMALY_05000380 && defined(CONFIG_BF52x) && defined(CONFIG_USB_MUSB_HDRC) && !defined(CONFIG_MUSB_PIO_ONLY)
-#error Please use PIO mode in MUSB driver on bf52x chip v0.0 and v0.1.
-#endif
-
