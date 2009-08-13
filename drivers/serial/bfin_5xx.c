2cbcd9437c9c97a0ed7a7978de9eef05e04a88cc
Sonic Zhang <sonic.zhang@analog.com>
2009-08-13 03:07:11 +0000

    Bug[#5420] don't set CON_PRINTBUFFER for bfin serial console to avoid double line kernel boot information in bfin serial console.
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@7173 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/serial/bfin_5xx.c
+++ drivers/serial/bfin_5xx.c
@@ -1322,6 +1322,12 @@ struct console __init *bfin_earlyserial_init(unsigned int port,
 	struct bfin_serial_port *uart;
 	struct ktermios t;
 
+	/*
+	 * If we are using early serial, don't let the normal console rewind
+	 * log buffer, since that causes things to be printed multiple times
+	 */
+	bfin_serial_console.flags &= ~CON_PRINTBUFFER;
+
 	if (port == -1 || port >= nr_active_ports)
 		port = 0;
 	bfin_serial_init_ports();
