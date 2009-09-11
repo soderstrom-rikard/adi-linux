641641985a77b0f69a87a14c341a9b2c6680b4b3
Mike Frysinger <vapier@gentoo.org>
2009-09-11 14:06:35 +0000

    [#5521] prevent user_shm_unlock() from being called on non-hugetlb systems
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@7299 526b6c2d-f592-4532-a319-5dd88ccb003d

--- ipc/shm.c
+++ ipc/shm.c
@@ -411,8 +411,10 @@ static int newseg(struct ipc_namespace *ns, struct ipc_params *params)
 	return error;
 
 no_id:
+#ifdef CONFIG_HUGETLB_PAGE
 	if (shp->mlock_user)	/* shmflg & SHM_HUGETLB case */
 		user_shm_unlock(size, shp->mlock_user);
+#endif
 	fput(file);
 no_file:
 	security_shm_free(shp);
