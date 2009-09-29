0ae4ed368b47071100d4396bd91d6a5bcf2199f5
Michael Hennerich <michael.hennerich@analog.com>
2009-09-29 09:34:24 +0000

    Address BUG[#5574] adv7393 driver doesn't work in trunk now on bf537-stamp Apply patch: [Uclinux-dist-devel] [PATCH] blackfin: fix framebuffer mmap bug fornommu
    The patch added a special get_unmapped_area for framebuffer which was
    hooked to the file ops in drivers/video/fbmem.c.
    
    This is needed since v2.6.29-rc1 where nommu vma management was updated,
    and mmap of framebuffer caused kernel BUG panic. You may turn on "Debug
    the global anon/private NOMMU mapping region tree" config to such
    message.
    
    As Documentation/nommu-mmap.txt said,
    "To provide shareable character device support, a driver must provide a
    file->f_op->get_unmapped_area() operation. The mmap() routines will call
    this to get a proposed address for the mapping."
    
    With this change, user space should call mmap for framebuffer using
    shared map. Or it can try shared map first, then private map if failed.
    This shared map usage is now consistent between mmu and nommu.
    
    The sys_ file may not be a good place for this patch. But there is a
    similar one for sparc. I tested a similar patch on nios2nommu, though I
    don't have a blackfin board to test.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>
    
    
    
    
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@7502 526b6c2d-f592-4532-a319-5dd88ccb003d

--- arch/blackfin/kernel/sys_bfin.c
+++ arch/blackfin/kernel/sys_bfin.c
@@ -69,3 +69,15 @@ asmlinkage void *sys_dma_memcpy(void *dest, const void *src, size_t len)
 {
 	return safe_dma_memcpy(dest, src, len);
 }
+
+#ifdef CONFIG_FB
+#include <linux/fb.h>
+unsigned long get_fb_unmapped_area(struct file *filp, unsigned long orig_addr,
+	unsigned long len, unsigned long pgoff, unsigned long flags) {
+
+	struct fb_info *info = filp->private_data;
+
+	return info->screen_base;
+}
+EXPORT_SYMBOL(get_fb_unmapped_area);
+#endif
