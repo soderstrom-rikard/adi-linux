5e58f977f1448f8cb37439a5ec940e44bf68473b
Mike Frysinger <vapier@gentoo.org>
2009-09-30 03:59:34 +0000

    [#5581] get_fb_unmapped_area: fix build warning and build failure when fb.ko is a module
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@7516 526b6c2d-f592-4532-a319-5dd88ccb003d

--- arch/blackfin/kernel/sys_bfin.c
+++ arch/blackfin/kernel/sys_bfin.c
@@ -70,13 +70,13 @@ asmlinkage void *sys_dma_memcpy(void *dest, const void *src, size_t len)
 	return safe_dma_memcpy(dest, src, len);
 }
 
-#ifdef CONFIG_FB
+#if defined(CONFIG_FB) || defined(CONFIG_FB_MODULE)
 #include <linux/fb.h>
 unsigned long get_fb_unmapped_area(struct file *filp, unsigned long orig_addr,
 	unsigned long len, unsigned long pgoff, unsigned long flags)
 {
 	struct fb_info *info = filp->private_data;
-	return info->screen_base;
+	return (unsigned long)info->screen_base;
 }
 EXPORT_SYMBOL(get_fb_unmapped_area);
 #endif
