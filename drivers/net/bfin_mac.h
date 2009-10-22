b40482c0f6f2f8c3e811aaf61fe433017b5301c8
Sonic Zhang <sonic.zhang@analog.com>
2009-10-22 10:31:18 +0000

    Disable hardware checksum for bug #5600 if writeback cache is enabled. Otherwize, corrupted RX packet will be sent up stack without error mark.
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@7702 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/net/bfin_mac.h
+++ drivers/net/bfin_mac.h
@@ -14,7 +14,14 @@
 #include <linux/clocksource.h>
 #include <linux/timecompare.h>
 
+/*
+ * Disable hardware checksum for bug #5600 if writeback cache is
+ * enabled. Otherwize, corrupted RX packet will be sent up stack
+ * without error mark.
+ */
+#ifndef CONFIG_BFIN_EXTMEM_WRITEBACK
 #define BFIN_MAC_CSUM_OFFLOAD
+#endif
 
 struct dma_descriptor {
 	struct dma_descriptor *next_dma_desc;
