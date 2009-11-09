590cb0fcf43a404fde5d62df9a96e170d875769d
Barry Song <barry.song@analog.com>
2009-11-09 10:15:41 +0000

    Clean-up bfin-can driver according to Mike's comments
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@7769 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/net/can/bfin-can.h
+++ drivers/net/can/bfin-can.h
@@ -13,115 +13,149 @@
 
 #include <asm/io.h>
 
-/* We use only one transmit object for all messages to be transmitted */
-#define TRANSMIT_OBJ		24
-#define RECEIVE_STD_OBJ 	0
-#define RECEIVE_EXT_OBJ 	4
-#define RECEIVE_RTR_OBJ 	8
-#define RECEIVE_EXT_RTR_OBJ 	12
-
-/* Mailbox acceptance mask registers */
-struct can_mask {
-	u16 aml;		/* mailbox acceptance mask low		*/
-	u16 dummy1;
-	u16 amh;		/* mailbox acceptance mask high		*/
-	u16 dummy2;
-};
-
-#define CAN_MASK \
-	((struct can_mask *)CAN_AM00L)
-
 /*
- * Macros to handle BlackFin CAN message objects
- *
- * Structure for a single CAN object
- * A total of 32 such object structures exists (starting at CAN_BASE + 0x200)
- * ( 0xFFC02C00 )
+ * bfin can private data
  */
-
-struct can_obj {
-	u16 msg[8];     	/* Message Data 0 .. 7   		*/
-	u16 dlc;		/* data length code			*/
-	u16 dummy1;
-	u16 tsv;		/* time stamp value			*/
-	u16 dummy2;
-	u16 id0;		/* MB ID0 register			*/
-	u16 dummy3;
-	u16 id1;		/* MB ID1 register			*/
-	u16 dummy4;
+struct bfin_can_priv {
+	struct can_priv can;	/* must be the first member */
+	struct sk_buff *echo_skb;
+	struct net_device *dev;
+	u32 membase;
+	int rx_irq;
+	int tx_irq;
+	int err_irq;
+	unsigned short *pin_list;
 };
 
-/* CAN object definition */
-#define CAN_OBJ  \
-	((struct can_obj *)CAN_MB00_DATA0)
+/*
+ * registers offset
+ */
+#define OFFSET_MB_MASK              0x100
+#define OFFSET_MASK_AML             0x0
+#define OFFSET_MASK_AMH             0x4
+#define OFFSET_MB_OBJ               0x200
+#define OFFSET_OBJ_DATA             0x0
+#define OFFSET_OBJ_DLC              0x10
+#define OFFSET_OBJ_ID0              0x18
+#define OFFSET_OBJ_ID1              0x1C
+#define OFFSET_CLOCK                0x80
+#define OFFSET_TIMING               0x84
+#define OFFSET_STATUS               0x8C
+#define OFFSET_CEC                  0x90
+#define OFFSET_GIS                  0x94
+#define OFFSET_GIM                  0x98
+#define OFFSET_CONTROL              0xA0
+#define OFFSET_ESR                  0xB4
+#define OFFSET_MBIM1                0x28
+#define OFFSET_MBIM2                0x68
+#define OFFSET_MC1                  0x0
+#define OFFSET_MC2                  0x40
+#define OFFSET_MD1                  0x4
+#define OFFSET_MD2                  0x44
+#define OFFSET_TRS2                 0x48
+#define OFFSET_MBTIF1               0x20
+#define OFFSET_MBTIF2               0x60
+#define OFFSET_MBRIF1               0x24
+#define OFFSET_MBRIF2               0x64
+
+#define can_membase(priv)  \
+	((priv)->membase)
+#define can_channel_membase(priv, channel) \
+	((priv)->membase + OFFSET_MB_OBJ + ((channel) << 5))
+#define can_mask_membase(priv, channel)  \
+	((priv)->membase + OFFSET_MB_MASK + ((channel) << 3))
 
 /*
- * macros to read/write can registers
+ * read/write CAN registers and messages
  */
 #define CAN_WRITE_REG(val, addr) \
 	writew((val), (u16 *)(addr))
 
 #define CAN_READ_REG(addr) \
 	readw((u16 *)(addr))
-/*
- * macros to read/write data length code
- */
-#define CAN_WRITE_DLC(channel, length) \
-	(CAN_WRITE_REG((length), &CAN_OBJ[channel].dlc))
 
-#define CAN_READ_DLC(channel) \
-	(CAN_READ_REG(&CAN_OBJ[channel].dlc))
+#define CAN_WRITE_CTRL(priv, off, val) \
+	CAN_WRITE_REG(val, can_membase((priv)) + (off))
 
-/*
- * CAN_READ_OID(obj) is a macro to read the CAN-ID of the specified object.
- * It delivers the value as 16 bit from the standard ID registers.
- */
-#define CAN_READ_OID(channel) \
-	((CAN_READ_REG(&CAN_OBJ[channel].id1) & 0x1ffc) >> 2)
+#define CAN_READ_CTRL(priv, off) \
+	CAN_READ_REG(can_membase((priv)) + (off))
 
-#define CAN_READ_XOID(channel) \
-	(((CAN_READ_REG(&CAN_OBJ[channel].id1) & 0x1fff) << 16) \
-	 + ((CAN_READ_REG(&CAN_OBJ[channel].id0))))
+#define CAN_WRITE_AML(priv, channel, aml) \
+	(CAN_WRITE_REG((aml), can_mask_membase(priv, channel) + OFFSET_MASK_AML))
 
-/*
- * CAN_WRITE_OID(obj, id) is a macro to write the CAN-ID
- * of the specified object with identifier id.
- * CAN_WRITE_XOID(obj, id) is a macro to write the extended CAN-ID
- */
-#define CAN_WRITE_OID(channel, id) \
-	CAN_WRITE_REG(((id) << 2) | AME, &CAN_OBJ[channel].id1)
+#define CAN_WRITE_AMH(priv, channel, amh) \
+	(CAN_WRITE_REG((amh), can_mask_membase(priv, channel) + OFFSET_MASK_AMH))
 
-#define CAN_WRITE_XOID(channel, id)  \
+#define CAN_WRITE_DLC(priv, channel, length) \
+	(CAN_WRITE_REG((length), can_channel_membase(priv, channel) + OFFSET_OBJ_DLC))
+
+#define CAN_READ_DLC(priv, channel) \
+	(CAN_READ_REG(can_channel_membase((priv), (channel)) + OFFSET_OBJ_DLC))
+
+#define CAN_READ_OID(priv, channel) \
+	((CAN_READ_REG(can_channel_membase((priv), (channel)) + OFFSET_OBJ_ID1) & 0x1ffc) >> 2)
+
+#define CAN_READ_XOID(priv, channel) \
+	(((CAN_READ_REG(can_channel_membase((priv), (channel)) + OFFSET_OBJ_ID1) & 0x1fff) << 16) \
+	 + ((CAN_READ_REG(can_channel_membase((priv), (channel)) + OFFSET_OBJ_ID0))))
+
+#define CAN_READ_ID1(priv, channel) \
+	(CAN_READ_REG(can_channel_membase((priv), (channel)) + OFFSET_OBJ_ID1))
+
+#define CAN_WRITE_ID0(priv, channel, val) \
+	CAN_WRITE_REG((val), can_channel_membase((priv), (channel)) + OFFSET_OBJ_ID0)
+
+#define CAN_WRITE_OID(priv, channel, id) \
+	CAN_WRITE_REG(((id) << 2) | AME, can_channel_membase((priv), (channel)) + OFFSET_OBJ_ID1)
+
+#define CAN_WRITE_XOID(priv, channel, id)  \
 	do { \
-		CAN_WRITE_REG((id), &CAN_OBJ[channel].id0); \
-		CAN_WRITE_REG((((id) & 0x1FFF0000) >> 16) + IDE + AME, &CAN_OBJ[channel].id1); \
+		CAN_WRITE_REG((id), can_channel_membase((priv), (channel)) + OFFSET_OBJ_ID0); \
+		CAN_WRITE_REG((((id) & 0x1FFF0000) >> 16) + IDE + AME, \
+				can_channel_membase((priv), (channel)) + OFFSET_OBJ_ID1); \
 	} while (0)
 
-/*
- * CAN_WRITE_OID_RTR(obj, id) is a macro to write the CAN-ID
- * of the specified object with identifier id and set the RTR Bit.
- */
-#define CAN_WRITE_OID_RTR(channel, id) \
-	CAN_WRITE_REG(((id) << 2) | RTR | AME, &CAN_OBJ[channel].id1)
+#define CAN_WRITE_OID_RTR(priv, channel, id) \
+	CAN_WRITE_REG(((id) << 2) | RTR | AME, can_channel_membase((priv), (channel)) + OFFSET_OBJ_ID1)
 
-#define CAN_WRITE_XOID_RTR(channel, id)  \
+#define CAN_WRITE_XOID_RTR(priv, channel, id)  \
 	do { \
-		CAN_WRITE_REG((id), &CAN_OBJ[channel].id0); \
-		CAN_WRITE_REG((((id) & 0x1FFF0000) >> 16) + IDE + RTR + AME, &CAN_OBJ[channel].id1); \
+		CAN_WRITE_REG((id), can_channel_membase((priv), (channel)) + OFFSET_OBJ_ID0); \
+		CAN_WRITE_REG((((id) & 0x1FFF0000) >> 16) + IDE + RTR + AME, \
+				can_channel_membase((priv), (channel)) + OFFSET_OBJ_ID1); \
 	} while (0)
 
-/*
- * bfin can private data
- */
+inline void BFIN_CAN_WRITE_MSG(struct bfin_can_priv *priv, int channel, u8 *data, int dlc)
+{
+	int i;
+	u16 val;
 
-struct bfin_can_priv {
-	struct can_priv can;	/* must be the first member */
-	struct sk_buff *echo_skb;
-	struct net_device *dev;
-	int rx_irq;
-	int tx_irq;
-	int err_irq;
-	unsigned short *pin_list;
-};
+	for (i = 0; i < 8; i += 2) {
+		val = ((7 - i) < dlc ? (data[7 - i]) : 0) +
+			((6 - i) < dlc ? (data[6 - i] << 8) : 0);
+		CAN_WRITE_REG(val, can_channel_membase((priv), (channel)) + OFFSET_OBJ_DATA + (i << 1));
+	}
+}
+
+inline void BFIN_CAN_READ_MSG(struct bfin_can_priv *priv, int channel, u8 *data, int dlc)
+{
+	int i;
+	u16 val;
+
+	for (i = 0; i < 8; i += 2) {
+		val = CAN_READ_REG(can_channel_membase((priv), (channel)) + OFFSET_OBJ_DATA + (i << 1));
+		data[7 - i] = (7 - i) < dlc ? val : 0;
+		data[6 - i] = (6 - i) < dlc ? (val >> 8) : 0;
+	}
+}
+
+/*
+ * transmit and receive channels
+ */
+#define TRANSMIT_CHL		24
+#define RECEIVE_STD_CHL 	0
+#define RECEIVE_EXT_CHL 	4
+#define RECEIVE_RTR_CHL 	8
+#define RECEIVE_EXT_RTR_CHL 	12
 
 #endif 		/* __BLACKFIN_CAN_H */
