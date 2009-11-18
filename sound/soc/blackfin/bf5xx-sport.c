8f2854c94d5af6b1c00e8cef2e1eb0bf3c8f32bf
Barry Song <barry.song@analog.com>
2009-11-18 03:32:59 +0000

    Fix compiling error for bfin-sport in trunk head
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@7839 526b6c2d-f592-4532-a319-5dd88ccb003d

--- sound/soc/blackfin/bf5xx-sport.c
+++ sound/soc/blackfin/bf5xx-sport.c
@@ -34,6 +34,7 @@
 #include <linux/gpio.h>
 #include <linux/bug.h>
 #include <asm/portmux.h>
+#include <asm/bfin_sport.h>
 #include <asm/dma.h>
 #include <asm/blackfin.h>
 #include <asm/cacheflush.h>
