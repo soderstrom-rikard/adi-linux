f330357cefab694bab296a61acbc38b7d9631f0d
Mike Frysinger <vapier@gentoo.org>
2009-12-21 16:33:00 +0000

    bfin_twi_lcd: clean up a bit
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@8058 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/char/bfin_twi_lcd.c
+++ drivers/char/bfin_twi_lcd.c
@@ -1,51 +1,169 @@
 /*
- * File:         drivers/char/bfin_twi_lcd.c
- * Based on:
- * Author:       Michael Hennerich
+ * TWI LCD driver (HD44780) connected to a PCF8574 I2C IO expander
  *
- * Created:      Feb. 27th 2006
- * Description:  TWI LCD driver (HD44780) connected to a PCF8574 I2C IO expander
- *
- * Modified:
- *               Copyright 2006 Analog Devices Inc.
- *
- * Bugs:         Enter bugs at http://blackfin.uclinux.org/
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see the file COPYING, or write
- * to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * Copyright 2006-2009 Analog Devices Inc.
+ *
+ * Licensed under the GPL-2 or later.
  */
 
+#define pr_fmt(x) "bfin_twi_lcd: " x
+
+#include <linux/delay.h>
 #include <linux/types.h>
 #include <linux/errno.h>
 #include <linux/miscdevice.h>
 #include <linux/slab.h>
+#include <linux/i2c.h>
+#include <linux/io.h>
 #include <linux/ioport.h>
 #include <linux/fcntl.h>
 #include <linux/fs.h>
 #include <linux/sched.h>
-#include <linux/delay.h>
 
-#include <asm/io.h>
-#include <asm/uaccess.h>
-#include <asm/system.h>
-#include <linux/delay.h>
-#include <linux/i2c.h>
+/* HD44780 execution timings [microseconds]
+ * as these values differ from spec to spec,
+ * we use the worst-case values.
+ */
 
-#include "bfin_twi_lcd.h"
+#define T_INIT1    5		/* (ms) first init sequence:  4.1 msec */
+#define T_INIT2  150		/* second init sequence: 100 usec */
+#define T_EXEC    80		/* normal execution time */
+#define T_WRCG   120		/* CG RAM Write */
+#define T_CLEAR    3		/* (ms) Clear Display */
 
-static int __init pcf8574_lcd_probe(struct i2c_client *client, const struct i2c_device_id *id)
+#define SIGNAL_RW		0x20
+#define SIGNAL_RS		0x10
+#define SIGNAL_ENABLE 	0x40
+#define SIGNAL_ENABLE2	0x80
+
+/* LCD Driver function headers and globals */
+
+#define	PCF8574_LCD_DRV_NAME		"pcf8574_lcd"
+static struct i2c_client *pcf8574_lcd_client;
+
+static int currController = 0x2;
+static unsigned int lcd_present = 0;
+
+#define kLCD_Addr       0x80
+
+
+#define BusyCheck()	do { } while (0)
+
+/*
+ * Function command codes for io_ctl.
+ */
+#define LCD_On			1
+#define LCD_Off			2
+#define LCD_Clear		3
+#define LCD_Reset		4
+#define LCD_Cursor_Left		5
+#define LCD_Cursor_Right	6
+#define LCD_Disp_Left		7
+#define LCD_Disp_Right		8
+#define LCD_Set_Cursor		10
+#define LCD_Home		11
+#define LCD_Curr_Controller	12
+#define LCD_Cursor_Off		14
+#define LCD_Cursor_On		15
+#define LCD_Set_Cursor_Pos	17
+#define LCD_Blink_Off           18
+#define LCD_Contr           19
+
+#define CONTROLLER_1	0x1
+#define CONTROLLER_2	0x2
+#define CONTROLLER_BOTH	0x3
+
+static void drv_HD_I2C_nibble(unsigned char controller, unsigned char nibble)
+{
+	unsigned char enable;
+	unsigned char command;	/* this is actually the first data byte on the PCF8574 */
+	unsigned char data_block[2];
+	/* enable signal: 'controller' is a bitmask */
+	/* bit n .. send to controller #n */
+	/* so we can send a byte to more controllers at the same time! */
+	enable = 0;
+	if (controller & 0x01)
+		enable |= SIGNAL_ENABLE;
+	if (controller & 0x02)
+		enable |= SIGNAL_ENABLE2;
+
+	command = nibble;
+	data_block[0] = nibble | enable;
+	data_block[1] = nibble;
+
+	i2c_smbus_write_block_data(pcf8574_lcd_client, command, 2, data_block);
+}
+
+static void drv_HD_I2C_byte(const unsigned char controller,
+			    const unsigned char data)
+{
+	/* send data with RS enabled */
+	drv_HD_I2C_nibble(controller, ((data >> 4) & 0x0f) | SIGNAL_RS);
+	drv_HD_I2C_nibble(controller, (data & 0x0f) | SIGNAL_RS);
+	udelay(T_INIT2);
+}
+
+static void drv_HD_I2C_command(const unsigned char controller,
+			       const unsigned char cmd)
+{
+	/* send data with RS disabled */
+	drv_HD_I2C_nibble(controller, ((cmd >> 4) & 0x0f));
+	drv_HD_I2C_nibble(controller, ((cmd) & 0x0f));
+	udelay(T_INIT2);
+}
+
+static void drv_HD_I2C_data(const unsigned char controller, const char *string,
+			    const int len)
+{
+	int l = len;
+
+	/* sanity check */
+	if (len <= 0)
+		return;
+
+	while (l--) {
+		if (*string)
+			drv_HD_I2C_byte(controller, *(string++));
+	}
+}
+
+static int drv_HD_I2C_load(void)
+{
+	/* initialize display */
+	drv_HD_I2C_nibble(CONTROLLER_BOTH, 0x03);
+	msleep(T_INIT1);	/* 4 Bit mode, wait 4.1 ms */
+	drv_HD_I2C_nibble(CONTROLLER_BOTH, 0x03);
+	udelay(T_INIT2);	/* 4 Bit mode, wait 100 us */
+	drv_HD_I2C_nibble(CONTROLLER_BOTH, 0x03);
+	udelay(T_INIT2);	/* 4 Bit mode, wait 4.1 ms */
+	drv_HD_I2C_nibble(CONTROLLER_BOTH, 0x02);
+	udelay(T_INIT2);	/* 4 Bit mode, wait 100 us */
+	drv_HD_I2C_command(CONTROLLER_BOTH, 0x28);	/* 4 Bit mode, 1/16 duty cycle, 5x8 font */
+
+	/* Set defaults:
+	   Low 0xC0 Display On
+	   Low 0x06 Cursor increment, no shift
+	   Low 0x80 Display Address 0
+	   Low 0x02 Cursor Home */
+
+	udelay(T_INIT2);
+	drv_HD_I2C_command(CONTROLLER_BOTH, 0x0f);
+	udelay(T_INIT2);
+	drv_HD_I2C_command(CONTROLLER_BOTH, 0x06);
+	udelay(T_INIT2);
+	drv_HD_I2C_command(CONTROLLER_BOTH, 0x80);
+	udelay(T_INIT2);
+	drv_HD_I2C_command(CONTROLLER_BOTH, 0x02);
+	udelay(T_INIT2);
+	drv_HD_I2C_command(CONTROLLER_BOTH, 0x0C);
+	udelay(T_INIT2);
+	drv_HD_I2C_command(CONTROLLER_BOTH, 0x01);
+	udelay(T_INIT2);
+
+	return 0;
+}
+
+static int __devinit pcf8574_lcd_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
 	pcf8574_lcd_client = client;
 	drv_HD_I2C_load();
@@ -54,7 +172,7 @@ static int __init pcf8574_lcd_probe(struct i2c_client *client, const struct i2c_
 	return 0;
 }
 
-static int __exit pcf8574_lcd_remove(struct i2c_client *client)
+static int __devexit pcf8574_lcd_remove(struct i2c_client *client)
 {
 	return 0;
 }
@@ -70,7 +188,7 @@ static struct i2c_driver pcf8574_lcd_driver = {
 		.name = PCF8574_LCD_DRV_NAME,
 	},
 	.probe = pcf8574_lcd_probe,
-	.remove = __exit_p(pcf8574_lcd_remove),
+	.remove = __devexit_p(pcf8574_lcd_remove),
 	.id_table = pcf8574_lcd_id,
 };
 
@@ -209,144 +327,49 @@ static int lcd_open(struct inode *inode, struct file *file)
 static ssize_t lcd_write(struct file *filp, const char *buf, size_t count,
 			 loff_t * f_pos)
 {
-
 	drv_HD_I2C_data(currController, buf, count);
 
 	return count;
-
 }
 
 /*
  *	The various file operations we support.
  */
 
-static struct file_operations lcd_fops = {
-      owner:THIS_MODULE,
-//      read:lcd_read,
-      write:lcd_write,
-      ioctl:lcd_ioctl,
-      open:lcd_open,
+static const struct file_operations lcd_fops = {
+	.owner = THIS_MODULE,
+	.write = lcd_write,
+	.ioctl = lcd_ioctl,
+	.open  = lcd_open,
 };
 
 static struct miscdevice bfin_twi_lcd_dev = {
-	LCD_MINOR,
-	"lcd",
-	&lcd_fops
+	.minor = MISC_DYNAMIC_MINOR,
+	.name  = "lcd",
+	.fops  = &lcd_fops
 };
 
 static int __init lcd_init(void)
 {
 	int result;
-	pr_info("%s\n", LCD_DRIVER);
+	pr_info("loaded\n");
 
 	result = misc_register(&bfin_twi_lcd_dev);
 	if (result < 0) {
-		printk(KERN_WARNING "bfin_twi_lcd: can't get minor %d\n",
-		       MISC_MAJOR);
+		pr_err("unable to register misc device\n");
 		return result;
 	}
 
 	return i2c_add_driver(&pcf8574_lcd_driver);
 }
-
-static void drv_HD_I2C_nibble(unsigned char controller, unsigned char nibble)
-{
-	unsigned char enable;
-	unsigned char command;	/* this is actually the first data byte on the PCF8574 */
-	unsigned char data_block[2];
-	/* enable signal: 'controller' is a bitmask */
-	/* bit n .. send to controller #n */
-	/* so we can send a byte to more controllers at the same time! */
-	enable = 0;
-	if (controller & 0x01)
-		enable |= SIGNAL_ENABLE;
-	if (controller & 0x02)
-		enable |= SIGNAL_ENABLE2;
-
-	command = nibble;
-	data_block[0] = nibble | enable;
-	data_block[1] = nibble;
-
-	i2c_smbus_write_block_data(pcf8574_lcd_client, command, 2, data_block);
-}
-
-static void drv_HD_I2C_byte(const unsigned char controller,
-			    const unsigned char data)
-{
-	/* send data with RS enabled */
-	drv_HD_I2C_nibble(controller, ((data >> 4) & 0x0f) | SIGNAL_RS);
-	drv_HD_I2C_nibble(controller, (data & 0x0f) | SIGNAL_RS);
-	udelay(T_INIT2);
-}
-
-static void drv_HD_I2C_command(const unsigned char controller,
-			       const unsigned char cmd)
-{
-	/* send data with RS disabled */
-	drv_HD_I2C_nibble(controller, ((cmd >> 4) & 0x0f));
-	drv_HD_I2C_nibble(controller, ((cmd) & 0x0f));
-	udelay(T_INIT2);
-}
-
-static void drv_HD_I2C_data(const unsigned char controller, const char *string,
-			    const int len)
-{
-	int l = len;
-
-	/* sanity check */
-	if (len <= 0)
-		return;
-
-	while (l--) {
-		if (*string)
-			drv_HD_I2C_byte(controller, *(string++));
-	}
-}
-
-static int drv_HD_I2C_load(void)
-{
-	/* initialize display */
-	drv_HD_I2C_nibble(CONTROLLER_BOTH, 0x03);
-	msleep(T_INIT1);	/* 4 Bit mode, wait 4.1 ms */
-	drv_HD_I2C_nibble(CONTROLLER_BOTH, 0x03);
-	udelay(T_INIT2);	/* 4 Bit mode, wait 100 us */
-	drv_HD_I2C_nibble(CONTROLLER_BOTH, 0x03);
-	udelay(T_INIT2);	/* 4 Bit mode, wait 4.1 ms */
-	drv_HD_I2C_nibble(CONTROLLER_BOTH, 0x02);
-	udelay(T_INIT2);	/* 4 Bit mode, wait 100 us */
-	drv_HD_I2C_command(CONTROLLER_BOTH, 0x28);	/* 4 Bit mode, 1/16 duty cycle, 5x8 font */
-
-	/* Set defaults:
-	   Low 0xC0 Display On
-	   Low 0x06 Cursor increment, no shift
-	   Low 0x80 Display Address 0
-	   Low 0x02 Cursor Home */
-
-	udelay(T_INIT2);
-	drv_HD_I2C_command(CONTROLLER_BOTH, 0x0f);
-	udelay(T_INIT2);
-	drv_HD_I2C_command(CONTROLLER_BOTH, 0x06);
-	udelay(T_INIT2);
-	drv_HD_I2C_command(CONTROLLER_BOTH, 0x80);
-	udelay(T_INIT2);
-	drv_HD_I2C_command(CONTROLLER_BOTH, 0x02);
-	udelay(T_INIT2);
-	drv_HD_I2C_command(CONTROLLER_BOTH, 0x0C);
-	udelay(T_INIT2);
-	drv_HD_I2C_command(CONTROLLER_BOTH, 0x01);
-	udelay(T_INIT2);
-
-	return 0;
-}
+module_init(lcd_init);
 
 static void __exit lcd_exit(void)
 {
 	i2c_del_driver(&pcf8574_lcd_driver);
 	misc_deregister(&bfin_twi_lcd_dev);
-	printk(KERN_ALERT "Goodbye LCD\n");
+	pr_info("goodbye\n");
 }
-
-module_init(lcd_init);
 module_exit(lcd_exit);
 
 MODULE_AUTHOR("Michael Hennerich <hennerich@blackfin.uclinux.org>");
