8df77182cd5d0d7f15c47425189434bc8066fa98
Mike Frysinger <vapier@gentoo.org>
2010-01-19 07:03:57 +0000

    adf7021: move mac storage into platform struct
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@8182 526b6c2d-f592-4532-a319-5dd88ccb003d

--- include/linux/spi/adf702x.h
+++ include/linux/spi/adf702x.h
@@ -5,7 +5,7 @@
  * and may vary between boards and models. The platform_data for the
  * device's "struct device" holds this information.
  *
- * Copyright 2009 Analog Devices Inc.
+ * Copyright 2009-2010 Analog Devices Inc.
  *
  * Licensed under the GPL-2 or later.
  */
@@ -13,11 +13,12 @@
 #ifndef __LINUX_SPI_ADF702X_H__
 #define __LINUX_SPI_ADF702X_H__
 
+#include <linux/if_ether.h>
+
 #define MODEL_ADF7021	7021
 #define MODEL_ADF7025	7025
 
 struct adf702x_platform_data {
-
 	/* Base reg base of SPORT controller */
 	void __iomem *regs_base;
 	unsigned dma_ch_rx;
@@ -29,6 +30,6 @@ struct adf702x_platform_data {
 	const u32 *adf702x_regs;
 	u32 tx_reg;
 	u32 adf7025_tclkdiv;
-	const u8 *mac_addr;
+	u8 mac_addr[ETH_ALEN];
 };
 #endif
