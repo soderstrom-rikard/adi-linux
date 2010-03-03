8a468b864fa69b7dfb8138699b880b967d6b2206
Michael Hennerich <michael.hennerich@analog.com>
2010-03-03 12:41:55 +0000

    Fix, too few arguments for format
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@8370 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/staging/iio/adc/ad7414.c
+++ drivers/staging/iio/adc/ad7414.c
@@ -187,7 +187,7 @@ static ssize_t ad7414_show_temperature(struct device *dev,
 	value = (s16)be16_to_cpu(data);
 	value >>= AD7414_TEMP_OFFSET;
 
-	return sprintf(buf, "%d.%d%d\n", (value >> 2), (value & 3) * 25);
+	return sprintf(buf, "%d.%d\n", (value >> 2), (value & 3) * 25);
 }
 
 IIO_DEVICE_ATTR(temperature, S_IRUGO, ad7414_show_temperature, NULL, 0);
