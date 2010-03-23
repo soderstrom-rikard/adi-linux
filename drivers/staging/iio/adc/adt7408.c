c725551fcebc310494fb74d744557656bc437797
Sonic Zhang <sonic.zhang@analog.com>
2010-03-23 07:42:39 +0000

    Task[#5912] Find variable of iio_work_cont by proper member in adt7408.
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@8534 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/staging/iio/adc/adt7408.c
+++ drivers/staging/iio/adc/adt7408.c
@@ -318,8 +318,7 @@ static const struct attribute_group adt7408_attribute_group = {
 
 static void adt7408_interrupt_bh(struct work_struct *work_s)
 {
-	struct iio_work_cont *wc
-		= container_of(work_s, struct iio_work_cont, ws_nocheck);
+	struct iio_work_cont *wc = to_iio_work_cont_check(work_s);
 	struct adt7408_chip_info *chip = wc->st;
 	u16 config;
 	u16 data;
