5b071a4f11432c9ed75a54989e98c7c9da6cb986
Barry Song <barry.song@analog.com>
2010-04-12 09:48:37 +0000

    [#5994] New IIO driver for raw input data stream only. Programable Digital Gyro adis1626x
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@8633 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/staging/iio/imu/gyro.h
+++ drivers/staging/iio/imu/gyro.h
@@ -3,6 +3,9 @@
 
 /* Gyroscope types of attribute */
 
+#define IIO_DEV_ATTR_GYRO_OFFSET(_mode, _show, _store, _addr)	\
+	IIO_DEVICE_ATTR(gyro_offset, _mode, _show, _store, _addr)
+
 #define IIO_DEV_ATTR_GYRO_X_OFFSET(_mode, _show, _store, _addr)	\
 	IIO_DEVICE_ATTR(gyro_x_offset, _mode, _show, _store, _addr)
 
@@ -20,6 +23,12 @@
 
 #define IIO_DEV_ATTR_GYRO_Z_GAIN(_mode, _show, _store, _addr)		\
 	IIO_DEVICE_ATTR(gyro_z_gain, _mode, _show, _store, _addr)
+
+#define IIO_DEV_ATTR_GYRO_SCALE(_mode, _show, _store, _addr)		\
+	IIO_DEVICE_ATTR(gyro_scale, S_IRUGO, _show, _store, _addr)
+
+#define IIO_DEV_ATTR_GYRO(_show, _addr)			\
+	IIO_DEVICE_ATTR(gyro, S_IRUGO, _show, NULL, _addr)
 
 #define IIO_DEV_ATTR_GYRO_X(_show, _addr)			\
 	IIO_DEVICE_ATTR(gyro_x, S_IRUGO, _show, NULL, _addr)
@@ -45,3 +54,5 @@
 #define IIO_DEV_ATTR_INCLI_Y(_show, _addr)			\
 	IIO_DEVICE_ATTR(incli_y, S_IRUGO, _show, NULL, _addr)
 
+#define IIO_DEV_ATTR_ANGL(_show, _addr)                         \
+	IIO_DEVICE_ATTR(angl, S_IRUGO, _show, NULL, _addr)
