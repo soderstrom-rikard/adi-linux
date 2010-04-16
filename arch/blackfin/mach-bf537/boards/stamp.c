35673cc640eceaadbc015548593c434bdecf660b
Barry Song <barry.song@analog.com>
2010-04-16 09:53:42 +0000

    [#5996] New IIO driver for raw input data stream only. Digital Output Gyro adis160xx and adis16100 ADIS16130 spi board information
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@8650 526b6c2d-f592-4532-a319-5dd88ccb003d

--- arch/blackfin/mach-bf537/boards/stamp.c
+++ arch/blackfin/mach-bf537/boards/stamp.c
@@ -1348,6 +1348,17 @@ static struct spi_board_info bfin_spi_board_info[] __initdata = {
 		.mode = SPI_MODE_1,
 	},
 #endif
+#if defined(CONFIG_ADIS16130) \
+	|| defined(CONFIG_ADIS16130_MODULE)
+	{
+		.modalias = "adis16130",
+		.max_speed_hz = 1000000,     /* max spi clock (SCK) speed in HZ */
+		.bus_num = 0,
+		.chip_select = 1, /* CS for read, change it for your board */
+		.platform_data = NULL, /* No spi_driver specific config */
+		.mode = SPI_MODE_3,
+	},
+#endif
 #if defined(CONFIG_ADIS16260) \
 	|| defined(CONFIG_ADIS16260_MODULE)
 	{
