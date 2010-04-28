8eae35dc56b4e9773bbda3136f5e552343190a22
Barry Song <barry.song@analog.com>
2010-04-28 07:46:14 +0000

    [#6000] New IIO driver for programmable accelerometer impact sensor adis16240
    add spi board info
    
    
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@8674 526b6c2d-f592-4532-a319-5dd88ccb003d

--- arch/blackfin/mach-bf537/boards/stamp.c
+++ arch/blackfin/mach-bf537/boards/stamp.c
@@ -1396,6 +1396,18 @@ static struct spi_board_info bfin_spi_board_info[] __initdata = {
 		.irq = IRQ_PF4,
 	},
 #endif
+#if defined(CONFIG_ADIS16240) \
+	|| defined(CONFIG_ADIS16240_MODULE)
+	{
+		.modalias = "adis16240",
+		.max_speed_hz = 2000000,     /* max spi clock (SCK) speed in HZ */
+		.bus_num = 0,
+		.chip_select = 5, /* CS, change it for your board */
+		.platform_data = NULL, /* No spi_driver specific config */
+		.mode = SPI_MODE_3,
+		.irq = IRQ_PF4,
+	},
+#endif
 #if defined(CONFIG_ADIS16260) \
 	|| defined(CONFIG_ADIS16260_MODULE)
 	{
