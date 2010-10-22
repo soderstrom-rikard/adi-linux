494bbbaec2f44cdabdf04e79a7ccfc14ae5ba3f0
Mike Frysinger <vapier@gentoo.org>
2010-10-22 00:30:26 +0000

    linux/bfin_mac.h: fix style/copyright
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@9273 526b6c2d-f592-4532-a319-5dd88ccb003d

--- include/linux/bfin_mac.h
+++ include/linux/bfin_mac.h
@@ -1,12 +1,13 @@
 /*
  * Blackfin On-Chip MAC Driver
  *
- * Copyright 2004-2007 Analog Devices Inc.
+ * Copyright 2004-2010 Analog Devices Inc.
  *
  * Enter bugs at http://blackfin.uclinux.org/
  *
  * Licensed under the GPL-2 or later.
  */
+
 #ifndef _LINUX_BFIN_MAC_H_
 #define _LINUX_BFIN_MAC_H_
 
