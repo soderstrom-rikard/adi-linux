af2f982635b67bf5daddeea5185a2df5ae6488b9
Michael Hennerich <michael.hennerich@analog.com>
2010-10-25 15:43:15 +0000

    staging: iio: Disable support for CONFIG_IIO_RING_BUFFER until implemented
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@9412 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/staging/iio/gyro/adis16060.h
+++ drivers/staging/iio/gyro/adis16060.h
@@ -35,7 +35,7 @@ struct adis16060_state {
 	struct mutex			buf_lock;
 };
 
-#ifdef CONFIG_IIO_RING_BUFFER
+#if defined(CONFIG_IIO_RING_BUFFER) && defined(THIS_HAS_RING_BUFFER_SUPPORT)
 /* At the moment triggers are only used for ring buffer
  * filling. This may change!
  */
