2663f50b007e00d3cc1df753b7ce1421e0632473
Michael Hennerich <michael.hennerich@analog.com>
2010-10-25 15:44:23 +0000

    staging: iio: Disable support for CONFIG_IIO_RING_BUFFER until implemented
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@9418 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/staging/iio/meter/ade7759.h
+++ drivers/staging/iio/meter/ade7759.h
@@ -60,7 +60,7 @@ struct ade7759_state {
 	u8				*rx;
 	struct mutex			buf_lock;
 };
-#ifdef CONFIG_IIO_RING_BUFFER
+#if defined(CONFIG_IIO_RING_BUFFER) && defined(THIS_HAS_RING_BUFFER_SUPPORT)
 /* At the moment triggers are only used for ring buffer
  * filling. This may change!
  */
