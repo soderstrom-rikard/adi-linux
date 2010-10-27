e57c4e10597e7bf2a1942d1423c91ada702e5ef7
Mike Frysinger <vapier@gentoo.org>
2010-10-27 22:02:24 +0000

    Blackfin: pll.h: unify PLL_CTL/VR_CTL write funcs
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@9461 526b6c2d-f592-4532-a319-5dd88ccb003d

--- arch/blackfin/include/mach-common/pll.h
+++ arch/blackfin/include/mach-common/pll.h
@@ -48,8 +48,7 @@ bfin_iwr_save(unsigned long niwr0, unsigned long niwr1, unsigned long niwr2,
 }
 #endif
 
-/* Writing to PLL_CTL initiates a PLL relock sequence */
-static inline void bfin_write_PLL_CTL(unsigned int val)
+static inline void _bfin_write_pll_relock(u32 addr, unsigned int val)
 {
 	unsigned long flags, iwr0, iwr1, iwr2;
 
@@ -60,7 +59,7 @@ static inline void bfin_write_PLL_CTL(unsigned int val)
 	/* Enable the PLL Wakeup bit in SIC IWR */
 	bfin_iwr_save(IWR_ENABLE(0), 0, 0, &iwr0, &iwr1, &iwr2);
 
-	bfin_write_PLL_CTL(val);
+	bfin_write16(addr, val);
 	SSYNC();
 	asm("IDLE;");
 
@@ -68,24 +67,16 @@ static inline void bfin_write_PLL_CTL(unsigned int val)
 	local_irq_restore_hw(flags);
 }
 
+/* Writing to PLL_CTL initiates a PLL relock sequence */
+static inline void bfin_write_PLL_CTL(unsigned int val)
+{
+	_bfin_write_pll_relock(PLL_CTL, val);
+}
+
 /* Writing to VR_CTL initiates a PLL relock sequence */
 static inline void bfin_write_VR_CTL(unsigned int val)
 {
-	unsigned long flags, iwr0, iwr1, iwr2;
-
-	if (val == bfin_read_VR_CTL())
-		return;
-
-	local_irq_save_hw(flags);
-	/* Enable the PLL Wakeup bit in SIC IWR */
-	bfin_iwr_save(IWR_ENABLE(0), 0, 0, &iwr0, &iwr1, &iwr2);
-
-	bfin_write_VR_CTL(val);
-	SSYNC();
-	asm("IDLE;");
-
-	bfin_iwr_restore(iwr0, iwr1, iwr2);
-	local_irq_restore_hw(flags);
+	_bfin_write_pll_relock(VR_CTL, val);
 }
 
 #endif
