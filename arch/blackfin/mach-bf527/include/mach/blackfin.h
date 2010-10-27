a00425d1410433c333a1a9041a582dc052589511
Mike Frysinger <vapier@gentoo.org>
2010-10-27 04:56:32 +0000

    Blackfin: bf52x: fix #endif typoe
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@9446 526b6c2d-f592-4532-a319-5dd88ccb003d

--- arch/blackfin/mach-bf527/include/mach/blackfin.h
+++ arch/blackfin/mach-bf527/include/mach/blackfin.h
@@ -25,7 +25,7 @@
 # include <asm/cdef_LPBlackfin.h>
 # if defined(CONFIG_BF523) || defined(CONFIG_BF522)
 #  include "cdefBF522.h"
-# endf
+# endif
 # if defined(CONFIG_BF525) || defined(CONFIG_BF524)
 #  include "cdefBF525.h"
 # endif
