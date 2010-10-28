0100916c50b167eaf3b5ff91634ba19bddc7a7cd
Michael Hennerich <michael.hennerich@analog.com>
2010-10-28 08:40:45 +0000

    video: backlight: adp8870: Fix warn_unused_result warning
    handle return value, strict_strtoul is declared with attribute warn_unused_result
    
    
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@9467 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/video/backlight/adp8870_bl.c
+++ drivers/video/backlight/adp8870_bl.c
@@ -563,8 +563,10 @@ static ssize_t adp8870_bl_l1_daylight_max_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count)
 {
 	struct adp8870_bl *data = dev_get_drvdata(dev);
+	int ret = strict_strtoul(buf, 10, &data->cached_daylight_max);
+	if (ret)
+		return ret;
 
-	strict_strtoul(buf, 10, &data->cached_daylight_max);
 	return adp8870_store(dev, buf, count, ADP8870_BLMX1);
 }
 static DEVICE_ATTR(l1_daylight_max, 0664, adp8870_bl_l1_daylight_max_show,
