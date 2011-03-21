f926bddb43473cc7f82da0cd2d94f064c381a939
Mike Frysinger <vapier@gentoo.org>
2011-03-21 06:40:05 +0000

    revert atomic64_t ifdef checking as lkml wants a kconfig solution
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@9742 526b6c2d-f592-4532-a319-5dd88ccb003d

--- lib/atomic64_test.c
+++ lib/atomic64_test.c
@@ -12,8 +12,6 @@
 #include <linux/kernel.h>
 #include <asm/atomic.h>
 
-#ifdef ATOMIC64_INIT
-
 #define INIT(c) do { atomic64_set(&v, c); r = c; } while (0)
 static __init int test_atomic64(void)
 {
@@ -166,5 +164,3 @@ static __init int test_atomic64(void)
 }
 
 core_initcall(test_atomic64);
-
-#endif
