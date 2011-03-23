32f6909e0e3fb94b0719b6475afd5b80835ff17d
Mike Frysinger <vapier@gentoo.org>
2011-03-23 01:12:47 +0000

    Blackfin: bf54x: re-enable anomaly 05000353 for all revs
    Even though the anomaly sheet says that the the bootrom is fixed, tests
    have shown that the fix itself does not handle all cases.  So until we
    get a ROM update, assume the reset code is still broken and we need to
    handle things ourselves.
    
    
    git-svn-id: svn://localhost/svn/linux-kernel/trunk@9756 526b6c2d-f592-4532-a319-5dd88ccb003d

--- arch/blackfin/kernel/reboot.c
+++ arch/blackfin/kernel/reboot.c
@@ -34,11 +34,7 @@ static void bfin_reset(void)
 	 * automatically perform a software reset for us when
 	 * it starts executing after the core reset.
 	 */
-	/* force to execute this workaround since some chips can't
-	 * reset normally even though their anomaly lists don't
-	 * include ANOMALY_05000353 and ANOMALY_05000386.
-	 */
-	if (!((bfin_cpuid() == 0x27e4) && (bfin_revid() > 0))) {
+	if (ANOMALY_05000353 || ANOMALY_05000386) {
 		/* Initiate System software reset. */
 		bfin_write_SWRST(0x7);
 
