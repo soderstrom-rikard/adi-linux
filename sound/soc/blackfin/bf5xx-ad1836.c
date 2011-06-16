9b3b24f3abf08dea78a17596d492e7b7c3c7642c
Mike Frysinger <vapier@gentoo.org>
2011-06-16 18:38:32 -0400

    ASoC: AD1836: switch over to the code merged upstream
    The AD1836 driver wasn't renamed to AD183X, and a lot of work for adding
    new codecs was redone a bit.  So just sync directly to what is merged.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- sound/soc/blackfin/bf5xx-ad1836.c
+++ sound/soc/blackfin/bf5xx-ad1836.c
@@ -1,9 +1,9 @@
 /*
- * File:         sound/soc/blackfin/bf5xx-ad183x.c
+ * File:         sound/soc/blackfin/bf5xx-ad1836.c
  * Author:       Barry Song <Barry.Song@analog.com>
  *
  * Created:      Aug 4 2009
- * Description:  Board driver for ad183x sound chip
+ * Description:  Board driver for ad1836 sound chip
  *
  * Bugs:         Enter bugs at http://blackfin.uclinux.org/
  *
@@ -28,14 +28,14 @@
 #include <asm/dma.h>
 #include <asm/portmux.h>
 
-#include "../codecs/ad183x.h"
+#include "../codecs/ad1836.h"
 
 #include "bf5xx-tdm-pcm.h"
 #include "bf5xx-tdm.h"
 
-static struct snd_soc_card bf5xx_ad183x;
+static struct snd_soc_card bf5xx_ad1836;
 
-static int bf5xx_ad183x_hw_params(struct snd_pcm_substream *substream,
+static int bf5xx_ad1836_hw_params(struct snd_pcm_substream *substream,
 	struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
@@ -64,66 +64,66 @@ static int bf5xx_ad183x_hw_params(struct snd_pcm_substream *substream,
 	return 0;
 }
 
-static struct snd_soc_ops bf5xx_ad183x_ops = {
-	.hw_params = bf5xx_ad183x_hw_params,
+static struct snd_soc_ops bf5xx_ad1836_ops = {
+	.hw_params = bf5xx_ad1836_hw_params,
 };
 
-static struct snd_soc_dai_link bf5xx_ad183x_dai[] = {
+static struct snd_soc_dai_link bf5xx_ad1836_dai[] = {
 	{
-		.name = "ad183x",
-		.stream_name = "AD183X",
+		.name = "ad1836",
+		.stream_name = "AD1836",
 		.cpu_dai_name = "bfin-tdm.0",
-		.codec_dai_name = "ad183x-hifi",
+		.codec_dai_name = "ad1836-hifi",
 		.platform_name = "bfin-tdm-pcm-audio",
 		.codec_name = "spi0.4",
-		.ops = &bf5xx_ad183x_ops,
+		.ops = &bf5xx_ad1836_ops,
 	},
 	{
-		.name = "ad183x",
-		.stream_name = "AD183X",
+		.name = "ad1836",
+		.stream_name = "AD1836",
 		.cpu_dai_name = "bfin-tdm.1",
-		.codec_dai_name = "ad183x-hifi",
+		.codec_dai_name = "ad1836-hifi",
 		.platform_name = "bfin-tdm-pcm-audio",
 		.codec_name = "spi0.4",
-		.ops = &bf5xx_ad183x_ops,
+		.ops = &bf5xx_ad1836_ops,
 	},
 };
 
-static struct snd_soc_card bf5xx_ad183x = {
-	.name = "bfin-ad183x",
-	.dai_link = &bf5xx_ad183x_dai[CONFIG_SND_BF5XX_SPORT_NUM],
+static struct snd_soc_card bf5xx_ad1836 = {
+	.name = "bfin-ad1836",
+	.dai_link = &bf5xx_ad1836_dai[CONFIG_SND_BF5XX_SPORT_NUM],
 	.num_links = 1,
 };
 
-static struct platform_device *bfxx_ad183x_snd_device;
+static struct platform_device *bfxx_ad1836_snd_device;
 
-static int __init bf5xx_ad183x_init(void)
+static int __init bf5xx_ad1836_init(void)
 {
 	int ret;
 
-	bfxx_ad183x_snd_device = platform_device_alloc("soc-audio", -1);
-	if (!bfxx_ad183x_snd_device)
+	bfxx_ad1836_snd_device = platform_device_alloc("soc-audio", -1);
+	if (!bfxx_ad1836_snd_device)
 		return -ENOMEM;
 
-	platform_set_drvdata(bfxx_ad183x_snd_device, &bf5xx_ad183x);
-	ret = platform_device_add(bfxx_ad183x_snd_device);
+	platform_set_drvdata(bfxx_ad1836_snd_device, &bf5xx_ad1836);
+	ret = platform_device_add(bfxx_ad1836_snd_device);
 
 	if (ret)
-		platform_device_put(bfxx_ad183x_snd_device);
+		platform_device_put(bfxx_ad1836_snd_device);
 
 	return ret;
 }
 
-static void __exit bf5xx_ad183x_exit(void)
+static void __exit bf5xx_ad1836_exit(void)
 {
-	platform_device_unregister(bfxx_ad183x_snd_device);
+	platform_device_unregister(bfxx_ad1836_snd_device);
 }
 
-module_init(bf5xx_ad183x_init);
-module_exit(bf5xx_ad183x_exit);
+module_init(bf5xx_ad1836_init);
+module_exit(bf5xx_ad1836_exit);
 
 /* Module information */
 MODULE_AUTHOR("Barry Song");
-MODULE_DESCRIPTION("ALSA SoC AD183X board driver");
+MODULE_DESCRIPTION("ALSA SoC AD1836 board driver");
 MODULE_LICENSE("GPL");
 
