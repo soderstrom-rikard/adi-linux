48389a69773f1a9edfdf7f1698c28a9432b3869b
Sonic Zhang <sonic.zhang@analog.com>
2011-06-21 18:01:49 +0800

    spi:blackfin:Keep CS active in last transfer if flag cs_change is set.
    According to the definition of flag cs_change in spi.h, CS should keep active
    in last transfer of a message till the first transfer of next message if flag
    cs_change is set. This patch fix this bug in blackfin spi controller driver.
    
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- drivers/spi/spi_bfin5xx.c
+++ drivers/spi/spi_bfin5xx.c
@@ -870,7 +870,7 @@ static void bfin_spi_pump_transfers(unsigned long data)
 		message->actual_length += drv_data->len_in_bytes;
 		/* Move to next transfer of this msg */
 		message->state = bfin_spi_next_transfer(drv_data);
-		if (drv_data->cs_change)
+		if (drv_data->cs_change && message->state != DONE_STATE)
 			bfin_spi_cs_deactive(drv_data, chip);
 	}
 
