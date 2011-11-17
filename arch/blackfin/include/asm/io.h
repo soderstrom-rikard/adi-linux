4af8637315add570fc5b3d5b6e51fa8a6a904fc4
Steven Miao <realmz6@gmail.com>
2011-11-17 19:19:24 +0800

    bfin io: add platform specific __raw_writeb, __raw_writew, __raw_writel
    Signed-off-by: Steven Miao <realmz6@gmail.com>
    Signed-off-by: Bob Liu <lliubbo@gmail.com>

--- arch/blackfin/include/asm/io.h
+++ arch/blackfin/include/asm/io.h
@@ -10,6 +10,7 @@
 #include <linux/compiler.h>
 #include <linux/types.h>
 #include <asm/byteorder.h>
+#include <asm/blackfin.h>
 
 #define DECLARE_BFIN_RAW_READX(size, type, asm, asm_sign) \
 static inline type __raw_read##size(const volatile void __iomem *addr) \
@@ -32,6 +33,31 @@ DECLARE_BFIN_RAW_READX(w, u16, w, (z))
 #define __raw_readw __raw_readw
 DECLARE_BFIN_RAW_READX(l, u32, , )
 #define __raw_readl __raw_readl
+
+#define DECLARE_BFIN_RAW_WRITEX(size, type) \
+static inline void __raw_write##size(type val, volatile void __iomem *addr) \
+{ \
+	unsigned int tmp = 0; \
+	unsigned int tmp_addr = (unsigned int)addr; \
+	int is_smc = 0; \
+	if ((tmp_addr & ~(ASYNC_BANK0_SIZE - 1)) == ASYNC_BANK0_BASE) { \
+		tmp = bfin_read32(SMC_B0CTL); \
+		bfin_write32(SMC_B0CTL, tmp | 0x2000); \
+		SSYNC(); \
+		is_smc = 1; \
+	} \
+	*(volatile type __force *) addr = (type) val; \
+	if (is_smc) { \
+		tmp = bfin_read32(SMC_B0CTL); \
+		bfin_write32(SMC_B0CTL, tmp & ~0x2000); \
+	} \
+}
+DECLARE_BFIN_RAW_WRITEX(b, u8)
+#define __raw_writeb __raw_writeb
+DECLARE_BFIN_RAW_WRITEX(w, u16)
+#define __raw_writew __raw_writew
+DECLARE_BFIN_RAW_WRITEX(l, u32)
+#define __raw_writel __raw_writel
 
 extern void outsb(unsigned long port, const void *addr, unsigned long count);
 extern void outsw(unsigned long port, const void *addr, unsigned long count);
