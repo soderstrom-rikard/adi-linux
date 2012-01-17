865d1ef02b5eb204c7e9490d539cb0b41783c56d
Steven Miao <realmz6@gmail.com>
2012-01-17 17:03:33 +0800

    Revert "nohz: Remove tick_nohz_idle_enter_norcu() / tick_nohz_idle_exit_norcu()"
    This reverts commit 619fdbbb589a68163a25e0109c79ff5431f1fbc2.
    
    Signed-off-by: Steven Miao <realmz6@gmail.com>

--- arch/blackfin/kernel/process.c
+++ arch/blackfin/kernel/process.c
@@ -88,12 +88,10 @@ void cpu_idle(void)
 #endif
 		if (!idle)
 			idle = default_idle;
-		tick_nohz_idle_enter();
-		rcu_idle_enter();
+		tick_nohz_stop_sched_tick(1);
 		while (!need_resched())
 			idle();
-		rcu_idle_exit();
-		tick_nohz_idle_exit();
+		tick_nohz_restart_sched_tick();
 		preempt_enable_no_resched();
 		schedule();
 		preempt_disable();
