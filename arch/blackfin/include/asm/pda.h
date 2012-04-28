1ca215d7d31a449592ad92d5e518efb79ecdc97a
Steven Miao <realmz6@gmail.com>
2012-04-28 14:45:04 +0800

    Revert "Blackfin: pda: fix 4 byte memory corruption on UP systems"
    This reverts commit 3f3cb9fa94f3ffd28b281fa255178ddf9e480a25.
    GET_PDA macro will use next field in SMP kernel
    
    Signed-off-by: Steven Miao <realmz6@gmail.com>

--- arch/blackfin/include/asm/pda.h
+++ arch/blackfin/include/asm/pda.h
@@ -13,6 +13,8 @@
 #ifndef __ASSEMBLY__
 
 struct blackfin_pda {			/* Per-processor Data Area */
+	struct blackfin_pda *next;
+
 	unsigned long syscfg;
 #ifdef CONFIG_SMP
 	unsigned long imask;		/* Current IMASK value */
