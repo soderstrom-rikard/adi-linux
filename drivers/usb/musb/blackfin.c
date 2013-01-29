a19f2c82b377a648f37b79ed7a8820493d252448
Bob Liu <lliubbo@gmail.com>
2013-01-29 10:46:18 +0800

    drivers: musb: make device can be recognized twice
    USB host device can't be recognized after unplug.
    This patch fix it by reinit vbus register and state.
    
    Signed-off-by: Bob Liu <lliubbo@gmail.com>

--- drivers/usb/musb/blackfin.c
+++ drivers/usb/musb/blackfin.c
@@ -192,7 +192,12 @@ static irqreturn_t blackfin_interrupt(int irq, void *__hci)
 		mod_timer(&musb_conn_timer, jiffies + TIMER_DELAY);
 		musb->a_wait_bcon = TIMER_DELAY;
 	}
-
+#ifdef CONFIG_BF60x
+	if (musb->int_usb & MUSB_INTR_DISCONNECT && is_host_active(musb)) {
+		musb->xceiv->state = OTG_STATE_B_IDLE;
+		bfin_write_USB_VBUS_CTL(0x00);
+	}
+#endif
 	spin_unlock_irqrestore(&musb->lock, flags);
 
 	return retval;
