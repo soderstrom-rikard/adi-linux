38b014b8c48ce1234b1bdcbe67ce6d6c1d852b8e
Sonic Zhang <sonic.zhang@analog.com>
2013-03-18 13:55:32 +0800

    crypto: remove duplicated code after updating to kernel 3.8
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- crypto/tcrypt.c
+++ crypto/tcrypt.c
@@ -1213,9 +1213,7 @@ static int do_test(int m)
 	case 109:
 		ret += tcrypt_test("vmac(aes)");
 		break;
-	case 110:
-		ret += tcrypt_test("hmac(crc32)");
-		break;
+
 	case 110:
 		ret += tcrypt_test("hmac(crc32)");
 		break;
