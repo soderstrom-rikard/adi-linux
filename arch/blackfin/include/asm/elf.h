0933109bf3272e29917695679e70e30f279528b6
Steven Miao <realmz6@gmail.com>
2013-04-27 05:49:21 -0400

    keep update with upstream
    blackfin: cleanup unused defination
    blackfin: remove SET_PERSONALITY
    
    Signed-off-by: Steven Miao <realmz6@gmail.com>

--- arch/blackfin/include/asm/elf.h
+++ arch/blackfin/include/asm/elf.h
@@ -132,15 +132,4 @@ do {											\
 
 #define ELF_PLATFORM  (NULL)
 
-#define SET_PERSONALITY(ex) \
-	set_personality(PER_LINUX | (current->personality & (~PER_MASK)))
-
-struct mm_struct;
-struct elf_fdpic_params;
-struct elf32_phdr;
-extern int elf_fdpic_plat_process_phdr(struct mm_struct *, struct elf_fdpic_params *,
-                                       struct elf32_phdr *, unsigned long *, unsigned long *);
-#define ELF_FDPIC_PLAT_PROCESS_PHDR(mm, params, phdr, maddr, disp) \
-	elf_fdpic_plat_process_phdr(mm, params, phdr, maddr, disp)
-
 #endif
