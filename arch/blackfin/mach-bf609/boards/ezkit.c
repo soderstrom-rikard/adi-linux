b504529406516dd20e4acb197db035256937a417
Sonic Zhang <sonic.zhang@analog.com>
2013-08-15 14:08:05 +0800

    blackfin: Add STMMAC platform data to enable dwmac1000 driver on BF60x.
    - Enable GMAC
    - Set propler DMA PBL
    - Disable DMA store and forward mode
    
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- arch/blackfin/mach-bf609/boards/ezkit.c
+++ arch/blackfin/mach-bf609/boards/ezkit.c
@@ -112,11 +112,18 @@ static struct stmmac_mdio_bus_data phy_private_data = {
 	.phy_mask = 1,
 };
 
+static struct stmmac_dma_cfg eth_dma_cfg = {
+	.pbl	= 2,
+};
+
 static struct plat_stmmacenet_data eth_private_data = {
+	.has_gmac = 1,
 	.bus_id   = 0,
 	.enh_desc = 1,
 	.phy_addr = 1,
 	.mdio_bus_data = &phy_private_data,
+	.dma_cfg  = &eth_dma_cfg,
+	.force_sf_dma_mode = -1,
 };
 
 static struct platform_device bfin_eth_device = {
